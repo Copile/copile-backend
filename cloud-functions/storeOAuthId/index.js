008f954d9cf61e4c7689ba095d14c932b7509e62
Vantria <s1spam118@gmail.com>
2023-09-07 16:35:09 +0100

    added disconnect functionality to storeOAuthId

--- cloud-functions/storeOAuthId/index.js
+++ cloud-functions/storeOAuthId/index.js
@@ -1,115 +1,152 @@
-const express = require('express');
-const { Firestore } = require('@google-cloud/firestore');
+const express = require("express");
+const { Firestore } = require("@google-cloud/firestore");
 const db = new Firestore();
-const applyMiddleware = require('./middleware');
+const applyMiddleware = require("./middleware");
 const app = express();
-const axios = require('axios');
+const axios = require("axios");
 
 applyMiddleware(app);
 
 const DISCORD_CLIENT_ID = process.env.DISCORD_CLIENT_ID;
-const DISCORD_CLIENT_SECRET =  process.env.DISCORD_CLIENT_SECRET;
+const DISCORD_CLIENT_SECRET = process.env.DISCORD_CLIENT_SECRET;
 // const REDIRECT_URI = 'https://us-central1-copile.cloudfunctions.net/storeOAuthId/OAuthCallback/discord';
 const REDIRECT_URI = process.env.REDIRECT_URI;
 
-app.get('/discord/connect', (req, res) => {
+app.get("/discord/connect", (req, res) => {
   const queryParams = new URLSearchParams({
     client_id: DISCORD_CLIENT_ID,
     redirect_uri: REDIRECT_URI,
-    response_type: 'code',
-    scope: 'identify',
+    response_type: "code",
+    scope: "identify",
   });
 
   const authUrl = `https://discord.com/api/oauth2/authorize?${queryParams.toString()}`;
-  
+
   // Redirect the user to the Discord authorization URL
   res.redirect(authUrl);
 });
 
-
-app.get('/OAuthCallback/discord', async (req, res) => {
+app.get("/OAuthCallback/discord", async (req, res) => {
   try {
-    const code = req.get('code')
-    const user = req.get('userId');
+    const code = req.get("code");
+    const user = req.get("userId");
 
     if (!code) {
-      return res.status(400).send('Code parameter missing.');
+      return res.status(400).send("Code parameter missing.");
     }
 
     if (!user) {
-      return res.status(400).send('User id missing.');
+      return res.status(400).send("User id missing.");
     }
 
     const tokenData = await exchangeCodeForToken(code);
 
     if (!tokenData) {
-      return res.status(500).send('Error exchanging code for token.');
+      return res.status(500).send("Error exchanging code for token.");
     }
 
     const discordUserData = await getDiscordUserData(tokenData.access_token);
 
     if (!discordUserData) {
-      return res.status(500).send('Error retrieving Discord user ID.');
+      return res.status(500).send("Error retrieving Discord user ID.");
     }
 
     await saveDiscordUserIdToFirestore(user, discordUserData);
 
-    return res.send('Discord ID saved successfully.');
+    return res.send("Discord ID saved successfully.");
   } catch (error) {
-    console.error('Error:', error);
-    return res.status(500).send('Error saving Discord ID.');
+    console.error("Error:", error);
+    return res.status(500).send("Error saving Discord ID.");
+  }
+});
+
+app.post("/disconnectSocial", async (req, res) => {
+  try {
+    const user = req.get("userId");
+    const { social } = req.body;
+
+    if (!user) {
+      return res.status(400).send("User id missing.");
+    }
+
+    if (!social || (social !== "telegram" && social !== "discord")) {
+      return res.status(400).send("Invalid social platform specified.");
+    }
+
+    await disconnectSocialFromFirestore(user, social);
+
+    return res.send(`${social} disconnected successfully.`);
+  } catch (error) {
+    console.error("Error:", error);
+    return res.status(500).send(`Error disconnecting ${social}.`);
   }
 });
 
 async function exchangeCodeForToken(code) {
   try {
-    const tokenResponse = await axios.post('https://discord.com/api/oauth2/token', 
+    const tokenResponse = await axios.post(
+      "https://discord.com/api/oauth2/token",
       new URLSearchParams({
         client_id: DISCORD_CLIENT_ID,
         client_secret: DISCORD_CLIENT_SECRET,
-        grant_type: 'authorization_code',
+        grant_type: "authorization_code",
         code,
-        redirect_uri: REDIRECT_URI
+        redirect_uri: REDIRECT_URI,
       }).toString(),
       {
         headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
-        }
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
       }
     );
 
     return tokenResponse.data;
   } catch (error) {
-    throw new Error('Token exchange failed.', error);
+    throw new Error("Token exchange failed.", error);
   }
 }
 
 async function getDiscordUserData(accessToken) {
   try {
-    const userResponse = await axios.get('https://discord.com/api/v10/users/@me', {
-      headers: {
-        Authorization: `Bearer ${accessToken}`
+    const userResponse = await axios.get(
+      "https://discord.com/api/v10/users/@me",
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
       }
-    });
+    );
 
     return {
       id: userResponse.data.id,
       username: userResponse.data.username,
-      avatar: `https://cdn.discordapp.com/avatars/${userResponse.data.id}/${userResponse.data.avatar}.png`
+      avatar: `https://cdn.discordapp.com/avatars/${userResponse.data.id}/${userResponse.data.avatar}.png`,
     };
   } catch (error) {
-    throw new Error('Error retrieving Discord user data.', error);
+    throw new Error("Error retrieving Discord user data.", error);
   }
 }
 
 async function saveDiscordUserIdToFirestore(user, discordUserData) {
-  const usersRef = db.collection('users');
+  const usersRef = db.collection("users");
   try {
     await usersRef.doc(user).update({
-      discord: discordUserData
+      discord: discordUserData,
     });
   } catch (error) {
-    console.error('Error:', error);
+    console.error("Error:", error);
+  }
+}
+
+async function disconnectSocialFromFirestore(user, social) {
+  const usersRef = db.collection("users");
+  try {
+    const updateObject = {};
+    updateObject[`${social}.id`] = "x";
+    await usersRef.doc(user).update(updateObject);
+  } catch (error) {
+    console.error("Error:", error);
+    throw new Error("Firestore update failed.");
   }
 }
 
