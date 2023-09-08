205b74d7dfcb5276488fcef3757f6581d7adcd77
Jan Tokic <jan.tokic@htl-wels.at>
2023-09-08 20:50:27 +0200

    - added telegram functionality

--- cloud-functions/storeOAuthId/index.js
+++ cloud-functions/storeOAuthId/index.js
@@ -9,24 +9,10 @@ applyMiddleware(app);
 
 const DISCORD_CLIENT_ID = process.env.DISCORD_CLIENT_ID;
 const DISCORD_CLIENT_SECRET = process.env.DISCORD_CLIENT_SECRET;
-// const REDIRECT_URI = 'https://us-central1-copile.cloudfunctions.net/storeOAuthId/OAuthCallback/discord';
-const REDIRECT_URI = process.env.DISCORD_REDIRECT_URI;
+const DISCORD_REDIRECT_URI = process.env.DISCORD_REDIRECT_URI;
+const TELEGRAM_BOT_KEY = process.env.KEY_TELEGRAM_BOT_KEY;
 
-app.get("/discord/connect", (req, res) => {
-  const queryParams = new URLSearchParams({
-    client_id: DISCORD_CLIENT_ID,
-    redirect_uri: REDIRECT_URI,
-    response_type: "code",
-    scope: "identify",
-  });
-
-  const authUrl = `https://discord.com/api/oauth2/authorize?${queryParams.toString()}`;
-
-  // Redirect the user to the Discord authorization URL
-  res.redirect(authUrl);
-});
-
-app.get("/OAuthCallback/discord", async (req, res) => {
+app.get("/callback/discord", async (req, res) => {
   try {
     const user = req.get("userId");
     const code = req.query.code;
@@ -48,15 +34,122 @@ app.get("/OAuthCallback/discord", async (req, res) => {
     const discordUserData = await getDiscordUserData(tokenData.access_token);
 
     if (!discordUserData) {
-      return res.status(500).send("Error retrieving Discord user ID.");
+      return res.status(500).send("Error retrieving Discord user data.");
     }
 
-    await saveDiscordUserIdToFirestore(user, discordUserData);
+    await saveUserDataToFirestore(user, discordUserData, "discord");
 
-    return res.send("Discord ID saved successfully.");
+    return res.send("Discord User data saved successfully.");
   } catch (error) {
     console.error("Error:", error);
-    return res.status(500).send("Error saving Discord ID.");
+    return res.status(500).send("Error saving Discord User data.");
+  }
+});
+
+app.post("/callback/telegram", async (req, res) => {
+  try {
+    const { message } = req.body;
+
+    if (!message) {
+      return res.status(200).send("No message received.");
+    }
+    token = message.text.split(" ")[1];
+
+    if (!token) {
+      return res.status(200).send("No token received.");
+    }
+
+    // get the firestore user id from the token
+    const userRef = db.collection("users").where("chatToken", "==", token);
+    const userSnapshot = await userRef.get();
+    if (userSnapshot.empty) {
+      return res.status(200).send("No user found.");
+    }
+    const user = userSnapshot.docs[0].account;
+
+    // get the telegram chat id from the message
+    const chat_id = message.chat.id;
+    if (!chat_id) {
+      return res.status(200).send("No chat id received.");
+    }
+
+    const telegram_id = message.from.id;
+    if (!telegram_id) {
+      return res.status(200).send("No id received.");
+    }
+    const userData = {
+      id: telegram_id,
+      chat_id: chat_id,
+    };
+
+    // Save the user ID to your Firestore or database here
+    await saveUserDataToFirestore(user, userData, "telegram");
+
+    // Send a message back to the user
+    await sendTelegramMessage(chat_id, "Notifications enabled.");
+
+    return res.status(200).send("Telegram user data saved successfully.");
+  } catch (error) {
+    console.error('Error:', error);
+    return res.status(500).send('Error storing Telegram user ID.');
+  }
+});
+
+async function sendTelegramMessage(chat_id, message) {
+
+  const sendMessageUrl = `https://api.telegram.org/bot${TELEGRAM_BOT_KEY}/sendMessage?chat_id=${chat_id}&text=${message}`;
+  axios.post(sendMessageUrl)
+    .then(response => {
+      console.log('Message sent successfully:', response.data);
+    })
+    .catch(error => {
+      console.error('Error sending message:', error);
+    });
+}
+
+app.post("storeChatToken", async (req, res) => {
+  try {
+    const user = req.get("userId");
+    if (!user) {
+      return res.status(400).send("User id missing.");
+    }
+    // generate a random token
+    const token = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+
+    // check if user already has a token
+    const userRef = db.collection("users").doc(user);
+    const userData = await userRef.get();
+    if (userData.exists && userData.data().chatToken) {
+      return res.status(400).send("User already has a token.");
+    }
+
+    // save token to Firestore
+    await saveUserDataToFirestore(user, token, "chatToken");
+
+    return res.send("Chat token saved successfully.");
+  } catch (error) {
+    console.error("Error:", error);
+    return res.status(500).send("Error saving chat token.");
+  }
+});
+
+app.get("getChatToken", async (req, res) => {
+  try {
+    const user = req.get("userId");
+    if (!user) {
+      return res.status(400).send("User id missing.");
+    }
+
+    const userRef = db.collection("users").doc(user);
+    const userData = await userRef.get();
+    if (!userData.exists || !userData.data().chatToken) {
+      return res.status(400).send("User does not have a token.");
+    }
+
+    return res.send(userData.data().chatToken);
+  } catch (error) {
+    console.error("Error:", error);
+    return res.status(500).send("Error retrieving chat token.");
   }
 });
 
@@ -91,7 +184,7 @@ async function exchangeCodeForToken(code) {
         client_secret: DISCORD_CLIENT_SECRET,
         grant_type: "authorization_code",
         code,
-        redirect_uri: REDIRECT_URI,
+        redirect_uri: DISCORD_REDIRECT_URI,
       }).toString(),
       {
         headers: {
@@ -131,14 +224,14 @@ async function getDiscordUserData(accessToken) {
   }
 }
 
-async function saveDiscordUserIdToFirestore(user, discordUserData) {
+async function saveUserDataToFirestore(user, userData, social) {
   const usersRef = db.collection("users");
   try {
     await usersRef.doc(user).update({
-      discord: discordUserData,
+      [`${social}`]: userData,
     });
   } catch (error) {
-    console.error("Error:", error);
+    console.error("Error saving ${social} user data to Firestore:", error);
   }
 }
 
@@ -154,4 +247,4 @@ async function disconnectSocialFromFirestore(user, social) {
   }
 }
 
-exports.OAuthCallback = app;
+exports.callback = app;
