cdb2de6642255224d7d07c747a5729924d974590
Jan Tokic <jan.tokic@htl-wels.at>
2023-09-11 19:42:55 +0200

    - logger adjustments

--- cloud-functions/OAuth/middleware/errorHandler.js
+++ cloud-functions/OAuth/middleware/errorHandler.js
@@ -1,15 +1,13 @@
 const errorHandler = (err, req, res, next) => {
-    req.logger.error(`Error from ${err.source || 'Unknown source'}: ${err.message}`);
-    console.log(err);
-    req.logger.info(err.stack);
     // Check if the error is from the telegramWebhook and respond with 200.
     if (err.source === "telegramWebhook") {
-      console.log("telegramWebhook");
-      return res.status(200).send('OK'); // Send a response to Telegram with a 200 status code
+      req.logger.info("telegramWebhook");
+      res.status(200).send('OK'); // Send a response to Telegram with a 200 status code
+    } else {
+    req.logger.error(`Error from ${err.source || 'Unknown source'}: ${err.message}`);
+      req.logger.info("normal error");
+      res.status(err.status || 500).send(err.message || 'Internal Server Error');
     }
-  
-    console.log("normal error");
-    res.status(err.status || 500).send(err.message || 'Internal Server Error');
   };
   
   module.exports = errorHandler;
