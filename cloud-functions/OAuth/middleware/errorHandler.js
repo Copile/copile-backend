ec5245fdb1e1e8dd9d17ac9c7819f6f8d97d7bcd
Jan Tokic <jan.tokic@htl-wels.at>
2023-09-12 19:32:47 +0200

    - fixed telegram webhook

--- cloud-functions/OAuth/middleware/errorHandler.js
+++ cloud-functions/OAuth/middleware/errorHandler.js
@@ -1,12 +1,9 @@
 const errorHandler = (err, req, res, next) => {
-    console.log("Error handler called");
     // Check if the error is from the telegramWebhook and respond with 200.
     if (err.source === "telegramWebhook") {
-      req.logger.info("telegramWebhook");
       res.status(200).send('OK'); // Send a response to Telegram with a 200 status code
     } else {
     req.logger.error(`Error from ${err.source || 'Unknown source'}: ${err.message}`);
-      req.logger.info("normal error");
       res.status(err.status || 500).send(err.message || 'Internal Server Error');
     }
   };
