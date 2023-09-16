081439acd392e13ff1113bb03e8ec1865eb8774d
Vantria <s1spam118@gmail.com>
2023-09-16 19:30:03 +0100

    updated admintrades binance pct calculation v4

--- cloud-functions/admintrades/index.js
+++ cloud-functions/admintrades/index.js
@@ -466,15 +466,30 @@ async function getBinanceTrades(apiKey, apiSecret, userId) {
         position.entryPrice = position.entryPrice;
         position.realised_pnl = "0"; // Binance does not provide realised PnL via API
         position.size = position.positionAmt;
+        // position.unrealised_pnl_pct = String(
+        //   (
+        //     (parseFloat(position.unRealizedProfit) /
+        //       (parseFloat(position.positionAmt) *
+        //         parseFloat(position.entryPrice))) *
+        //     100 *
+        //     parseFloat(position.leverage)
+        //   ).toFixed(2)
+        // );
+
+        let unrealised_pnl_pct = (
+          (parseFloat(position.unRealizedProfit) /
+            (parseFloat(position.positionAmt) *
+              parseFloat(position.entryPrice))) *
+          100 *
+          parseFloat(position.leverage)
+        ).toFixed(2);
+
         position.unrealised_pnl_pct = String(
-          (
-            (parseFloat(position.unRealizedProfit) /
-              (parseFloat(position.positionAmt) *
-                parseFloat(position.entryPrice))) *
-            100 *
-            parseFloat(position.leverage)
-          ).toFixed(2)
+          parseFloat(position.unRealizedProfit) < 0
+            ? -unrealised_pnl_pct
+            : unrealised_pnl_pct
         );
+
         return await mapPositionToTrade(
           position,
           userId,
