7e04abb7d21d47c91f36d9484683ca7c19214612
Vantria <s1spam118@gmail.com>
2023-09-26 18:15:23 +0100

    add experimental changes to getTestnetPositions

--- cloud-functions/admintrades/exchanges/testnet/positions.js
+++ cloud-functions/admintrades/exchanges/testnet/positions.js
@@ -19,13 +19,15 @@ async function getTestnetPositions(apiKey, apiSecret, traderId) {
     const client = new ContractClient({
       key: apiKey,
       secret: apiSecret,
-      strict_param_validation: true,
+      // strict_param_validation: true,
       testnet: true,
     });
 
-    const positionData = await client.getPositions({
-      settleCoin: "USDT",
-    });
+    // const positionData = await client.getPositions({
+    //   settleCoin: "USDT",
+    // });
+
+    const positionData = await client.getPositions();
 
     if (!positionData.result.list) {
       return [];
