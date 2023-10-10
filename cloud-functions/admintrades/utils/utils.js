7bbbaa8212ff21aee74fab4a3ab1c67a38660dd3
Jan Tokic <jan.tokic@gmail.com>
2023-10-10 14:13:54 +0200

    -removed console.logs

--- cloud-functions/admintrades/utils/utils.js
+++ cloud-functions/admintrades/utils/utils.js
@@ -142,12 +142,10 @@ async function getActiveOrders(
 }
 
 async function checkOrderStatus(activeOrders, orderID, price, exchange) {
-  console.log("orderID: ", orderID);
   let foundOrder = activeOrders.find((order) => {
     if (exchange === EXCHANGE.BINANCE && order.symbol === "ETHUSDT") {
       return String(order.stopPrice) === String(price);
     }
-    console.log("order.orderId: ", order.orderId);
     return String(order.orderId) === String(orderID);
   });
 
