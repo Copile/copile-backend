47a860a59978e9370e76c05a4420700cd3f1948a
Jan Tokic <jan.tokic@gmail.com>
2023-10-11 20:18:27 +0200

    - fix binance balance

--- cloud-functions/admintrades/exchanges/binance/balance.js
+++ cloud-functions/admintrades/exchanges/binance/balance.js
@@ -11,7 +11,8 @@ const CustomError = require("../../utils/error");
  */
 async function getBinanceBalance(apiKey, apiSecret) {
   try {
-    const usdtBalance = await getBalance(apiKey, apiSecret);
+    const balanceData = await getBalance(apiKey, apiSecret);
+    const usdtBalance = balanceData.find((asset) => asset.asset === "USDT");
     return usdtBalance ? String(usdtBalance.availableBalance) : "0";
   } catch (error) {
     // If it's already a custom error, throw it as-is
