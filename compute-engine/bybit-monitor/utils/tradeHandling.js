190be21b62b3066027bf00d7942505b5bbd9b09b
ALADIN847 <aldinmalic12@gmail.com>
2023-10-17 16:52:17 +0200

    Worked on tradeHandling

--- compute-engine/bybit-monitor/utils/tradeHandling.js
+++ compute-engine/bybit-monitor/utils/tradeHandling.js
@@ -1,15 +1,17 @@
-const { storeTrade, storeTP, storeSL, fetchLatestTradeDoc, getTradeInfo } = require('../firestore/firestore.js');
+const { storeTrade, storeTP, storeSL, fetchLatestTradeDoc, getTradeInfo, updateTradeQuantity, deleteOrder, deleteTpSlOrder, getSpecficOrder } = require('../firestore/firestore.js');
+const CustomError = require("../firestore/error.js");
 const { v4: uuidv4 } = require('uuid');
 
 const accountId = "duelendigerdreckigerbastard"
 
 async function tradeHandling(order) {
-    console.log(order);
-    switch(order.detection) {
-      case 'new_order':
-        await storeTrade(accountId, order);
-        break;
-      case 'new_stop_loss':
+    try {
+      console.log(order);
+      switch(order.detection) {
+        case 'new_order':
+          await storeTrade(accountId, order);
+          break;
+        case 'new_stop_loss':
           order.tradeId = await fetchLatestTradeDoc(accountId, order.symbol, "bybit", order.side == "Buy" ? "Sell" : "Buy");
           order.slDocumentId = String(uuidv4());
           order.slNumber = 1
@@ -19,7 +21,7 @@ async function tradeHandling(order) {
           console.log(accountId);
           await storeSL(accountId, order)
           break;
-      case 'new_take_profit':
+        case 'new_take_profit':
           order.tradeId = await fetchLatestTradeDoc(accountId, order.symbol, "bybit", order.side == "Buy" ? "Sell" : "Buy");
           tradeInfo = await getTradeInfo(accountId, order.tradeId)
           order.tpValue = order.entry
@@ -29,8 +31,39 @@ async function tradeHandling(order) {
           order.tpDocumentId = String(uuidv4());
           await storeTP(accountId, order)
           break;
-    }
-    return order
+        case 'partial_close':
+          order.tradeId = await fetchLatestTradeDoc(accountId, order.symbol, "bybit", order.side == "Buy" ? "Sell" : "Buy");
+          tradeInfo = await getTradeInfo(accountId, order.tradeId)
+          order.partialPercentage = parseFloat((order.quantity / tradeInfo.quantity).toFixed(2))
+          if (order.partialPercentage >= 1) {
+            await deleteOrder(accountId, order.tradeId);
+          } else {
+            await updateTradeQuantity(accountId, order.tradeId, tradeInfo.quantity - order.quantity) 
+          }
+          break;
+        case 'cancelled_order':
+          order.tradeId = await fetchLatestTradeDoc(accountId, order.symbol, "bybit", order.side);
+          await deleteOrder(accountId, order.tradeId);
+          break;
+        case 'cancelled_take_profit':
+          order.tradeId = await fetchLatestTradeDoc(accountId, order.symbol, "bybit", order.side == "Buy" ? "Sell" : "Buy");
+          tpDocument = await getSpecficOrder(accountId, order.tradeId, order.orderId, "tp")
+          await deleteTpSlOrder(accountId, order.tradeId, tpDocument.documentId, "tp")
+          break;
+        case 'cancelled_stop_loss':
+          order.tradeId = await fetchLatestTradeDoc(accountId, order.symbol, "bybit", order.side == "Buy" ? "Sell" : "Buy");
+          tpDocument = await getSpecficOrder(accountId, order.tradeId, order.orderId, "tp")
+          await deleteTpSlOrder(accountId, order.tradeId, tpDocument.documentId, "tp")
+          break;
+      }
+      return order
+  } catch(error) {
+    throw new CustomError({
+      message: `Error handling the trade: ${error.message}`,
+      status: 500,
+      source: "tradeHandling",
+    });
   }
+}
   
 module.exports = tradeHandling;
