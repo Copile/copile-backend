1378f18bb3275a2dca2c000f2c8eca2d5488289d
Jan Tokic <jan.tokic@gmail.com>
2023-10-23 00:04:16 +0200

    - added basic functionality

---
+++ cloud-functions/apiKeys/exchanges/binance/session.js
@@ -0,0 +1,39 @@
+const ExchangeSession = require("../exchangeSession");
+const CustomError = require("../../utils/error");
+const { getBinanceAPIPerms } = require("./apiConfig");
+
+/**
+ * Class representing a Binance exchange session.
+ * @extends ExchangeSession
+ */
+class BinanceSession extends ExchangeSession {
+  /**
+   * Create a new BinanceSession.
+   * @param {string} apiKey - User's API key for Binance.
+   * @param {string} apiSecret - User's API secret for Binance.
+   */
+  constructor(apiKey, apiSecret) {
+    super(apiKey, apiSecret);
+  }
+
+  /**
+   * Get orders from Binance for a specific trader.
+   * @returns {Promise<Array>} - Array of order objects.
+   */
+  async getAPIPerms() {
+    try {
+      return await getBinanceAPIPerms(this.apiKey, this.apiSecret);
+    } catch (error) {
+      if (error instanceof CustomError) {
+        throw error;
+      }
+      throw new CustomError({
+        message: `Failed to fetch Binance orders: ${error.message}`,
+        status: 500,
+        source: "getOrders",
+      });
+    }
+  }
+}
+
+module.exports = BinanceSession;
---
+++ cloud-functions/apiKeys/exchanges/sessionFactory.js
@@ -0,0 +1,27 @@
+const BinanceSession = require("./binance/session");
+const CustomError = require("../utils/error");
+
+/**
+ * Factory function to create an exchange session.
+ * @param {string} exchange - The name of the exchange ("kucoin", "binance", "bingx", "testnet").
+ * @param {string} apiKey - API key for the session.
+ * @param {string} apiSecret - API secret for the session.
+ * @param {string} [apiPassphrase] - API passphrase for the session (optional, only for KuCoin).
+ * @returns {Object} An instance of the relevant session class.
+ * @throws {CustomError} Throws a custom error if the exchange is unknown.
+ */
+function createSession(exchange, apiKey, apiSecret, apiPassphrase) {
+  switch (exchange) {
+    case "binance":
+      return new BinanceSession(apiKey, apiSecret);
+
+    default:
+      throw new CustomError({
+        message: "Unknown Exchange",
+        status: 400,
+        source: "createSession",
+      });
+  }
+}
+
+module.exports = createSession;
