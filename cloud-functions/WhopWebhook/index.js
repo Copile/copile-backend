679b244237bcf138c1d65e27aeb2c205e909ab63
Vantria <s1spam118@gmail.com>
2023-10-24 02:41:30 +0100

    updated /createLicense, /deleteLicense, /plans and /updateMargin

--- cloud-functions/WhopWebhook/index.js
+++ cloud-functions/WhopWebhook/index.js
@@ -1,46 +1,46 @@
-const Firestore = require('@google-cloud/firestore')
-const db = new Firestore
-const { createUserKey, deleteExchangeKey } = require('./encryption')
+const Firestore = require("@google-cloud/firestore");
+const db = new Firestore();
+const { createUserKey, deleteExchangeKey } = require("./encryption");
 const express = require("express");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 
 // default user data object
 const userdata = {
-    'account': "",
-    'exchanges': {
-        'bybit': {
-            "api_key": "x",
-            "api_secret": "x",
-        },
-        'kucoin': {
-            "api_key": "x",
-            "api_secret": "x",
-            "api_passphrase": "x",
-        },
-        'binance': {
-            "api_key": "x",
-            "api_secret": "x",
-        },
-        'bingx': {
-            "api_key": "x",
-            "api_secret": "x"
-        }
+  account: "",
+  exchanges: {
+    bybit: {
+      api_key: "x",
+      api_secret: "x",
     },
-    "telegram": "x",
-    "discord": "x"
-}
+    kucoin: {
+      api_key: "x",
+      api_secret: "x",
+      api_passphrase: "x",
+    },
+    binance: {
+      api_key: "x",
+      api_secret: "x",
+    },
+    bingx: {
+      api_key: "x",
+      api_secret: "x",
+    },
+  },
+  telegram: "x",
+  discord: "x",
+};
 
 // default plan data object
 const plandata = {
-    'product': "",
-    'license': "",
-    'account_id': "",
-    'margin': 'x',
-    'percentage': 'x',
-    'option': 'x',
-    'preferred_exchange': "x"
-}
+  product: "",
+  license: "",
+  account_id: "",
+  margin: "x",
+  percentage: "x",
+  option: "x",
+  preferred_exchange: "x",
+};
 
 // Async function to delete a document and its subcollections
 async function deleteDocumentAndSubcollections(documentRef) {
@@ -48,9 +48,9 @@ async function deleteDocumentAndSubcollections(documentRef) {
   const subcollections = await documentRef.listCollections();
 
   // Delete all documents within the subcollections
-  const deletePromises = subcollections.map(async subcollection => {
+  const deletePromises = subcollections.map(async (subcollection) => {
     const docs = await subcollection.listDocuments();
-    return Promise.all(docs.map(doc => doc.delete()));
+    return Promise.all(docs.map((doc) => doc.delete()));
   });
 
   // Wait for all subcollections to be deleted
@@ -62,95 +62,104 @@ async function deleteDocumentAndSubcollections(documentRef) {
 
 // Route to create license
 app.post("/createLicense", async (req, res) => {
-    try {
-        let userbody = req.body;
-        
-        if (userbody['action'] !== "membership.went_valid") {
-            return res.status(400).send(JSON.stringify({ error: "Invalid action" }));
-        }
+  try {
+    let userbody = req.body;
 
-        const user = userbody['data']['user']['id'];
-        const account_id = userbody['data']['id'];
-        const product = userbody['data']['product']['id'];
-        const product_name = userbody['data']['product']['name']
-        const plan = userbody['data']['plan']['id'];
-        const license = userbody['data']['license_key'];
-
-        const userRef = db.collection('users').doc(user);
-        const userSnapshot = await userRef.get();
-
-        if (userSnapshot.exists) {
-            plandata.product = product;
-            plandata.license = license;
-            plandata.product_name = product_name;
-            plandata.account_id = account_id;
-            await userRef.collection('plans').doc(product).set(plandata);
-            console.log("Added product for the user with the id: " + user);
-        } else {
-            userdata.account = user;
-            plandata.product = product;
-            plandata.product_name = product_name
-            plandata.license = license;
-            plandata.account_id = account_id;
-            
-            await userRef.set(userdata);
-            await userRef.collection('plans').doc(product).set(plandata);
-            await createUserKey(user);
-            console.log("Created user with the id: " + user);
-        }
-        return res.send(JSON.stringify({ status: 200 }));
-    } catch (error) {
-        console.log(error);
-        return res.status(500).send(JSON.stringify({ error: "Internal server error" }));
+    if (userbody["action"] !== "membership.went_valid") {
+      return res.status(400).send(JSON.stringify({ error: "Invalid action" }));
     }
+
+    const user = userbody["data"]["user"]["id"];
+    const account_id = userbody["data"]["id"];
+    const product_id = userbody["data"]["product"]["id"];
+    const product_name = userbody["data"]["product"]["name"];
+    const license = userbody["data"]["license_key"];
+
+    // Fetch the group and product from Firestore
+    const groupsRef = db.collection("groups");
+    const groupSnapshot = await groupsRef.where("product_id", "==", product_id).get();
+    const group = groupSnapshot.docs[0].data();
+    const productRef = groupSnapshot.docs[0].ref.collection("products").doc(product_id);
+    const product = (await productRef.get()).data();
+
+    const userRef = db.collection("users").doc(user);
+    const userSnapshot = await userRef.get();
+
+    if (userSnapshot.exists) {
+      // User exists, update the user's data
+      userdata.account = user;
+      userdata.group_id = group.id; // Associate the user with the group
+      await userRef.update(userdata);
+    } else {
+      // User does not exist, create a new user document
+      userdata.account = user;
+      userdata.group_id = group.id; // Associate the user with the group
+      await userRef.set(userdata);
+      await createUserKey(user);
+    }
+
+    // For each worker in the product, create a new document in the plans subcollection
+    const workersRef = productRef.collection("workers");
+    const workersSnapshot = await workersRef.get();
+    workersSnapshot.forEach(async (doc) => {
+      plandata.product = product_id;
+      plandata.product_name = product_name;
+      plandata.license = license;
+      plandata.account_id = account_id;
+      plandata.enabled = false; // Initialize as disabled
+      await userRef.collection("plans").doc(doc.id).set(plandata);
+    });
+
+    console.log("Created user with the id: " + user);
+    return res.send(JSON.stringify({ status: 200 }));
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send(JSON.stringify({ error: "Internal server error" }));
+  }
 });
 
-// Route to delete license
+// In this adjusted code, when a user's membership becomes invalid, the webhook deletes the product
+// and its workers from the user's plans. If the user has no other plans, the user is also deleted.
+
 app.post("/deleteLicense", async (req, res) => {
-    try {
-        let userbody = req.body;
-        let plans = [];
-        if (userbody["action"] == "membership.went_invalid") {
-        const user = userbody["data"]["user"]["id"];
-        const plan = userbody['data']['plan']['id'];
-        const product = userbody['data']['product']['id'];
-        const query = db.collection("users").doc(user).collection("plans");
-        const snapshot = await query.get();
-        snapshot.forEach((doc) => {
-            plan_id = doc.id;
-            plans.push(plan_id);
-        });
-        if (plans.length === 1) {
-            const documentRef = db.collection("users").doc(user);
-            await deleteDocumentAndSubcollections(documentRef);
-        } else {
-            const delete_plan = db
-            .collection("users")
-            .doc(user)
-            .collection("plans")
-            .doc(product)
-            .delete();
-        }
-        return res.send(JSON.stringify({ status: 200 }));
-        } else {
-        throw new Error("Invalid action specified in request");
-        }
-    } catch (error) {
-        console.log(error);
-        return res.status(500).send(JSON.stringify({ error: "Internal server error" }));
+  try {
+    let userbody = req.body;
+    if (userbody["action"] == "membership.went_invalid") {
+      const user = userbody["data"]["user"]["id"];
+      const product = userbody["data"]["product"]["id"];
+
+      const userRef = db.collection("users").doc(user);
+      const productRef = userRef.collection("plans").doc(product);
+
+      // Delete the product and its workers
+      await deleteDocumentAndSubcollections(productRef);
+
+      // Check if the user has any other plans
+      const plansSnapshot = await userRef.collection("plans").get();
+      if (plansSnapshot.empty) {
+        // If the user has no other plans, delete the user
+        await deleteDocumentAndSubcollections(userRef);
+      }
+
+      return res.send(JSON.stringify({ status: 200 }));
+    } else {
+      throw new Error("Invalid action specified in request");
     }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send(JSON.stringify({ error: "Internal server error" }));
+  }
 });
 
 app.get("/", (req, res) => {
-    res.send("Hello World");
-})
-
-app.get("*", (req, res) => {
-    return res.status(400).send("Not Authorized");
+  res.send("Hello World");
 });
 
+app.get("*", (req, res) => {
+  return res.status(400).send("Not Authorized");
+});
 
 // expose the express app as a cloud function
 module.exports = {
-    createLicense: app
-}
+  createLicense: app,
+};
--- cloud-functions/member/index.js
+++ cloud-functions/member/index.js
@@ -1,81 +1,92 @@
-const { Firestore } = require('@google-cloud/firestore');
+const { Firestore } = require("@google-cloud/firestore");
 const db = new Firestore();
-const { getPublicKey } = require('./encryption')
+const { getPublicKey } = require("./encryption");
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.post('/updateExchange', async (req, res) => {
-  const userId = req.get("x-forwarded-authorization").split(" ")[1]
+app.post("/updateExchange", async (req, res) => {
+  const userId = req.get("x-forwarded-authorization").split(" ")[1];
 
-  const userDoc = await db.collection('users').doc(userId).get();
+  const userDoc = await db.collection("users").doc(userId).get();
 
   if (!userDoc.exists) {
-    return res.status(404).json({success:false,  error: 'User not found' });
+    return res.status(404).json({ success: false, error: "User not found" });
   }
-
 
   const request_exchange = req.body.exchange;
   const exchange = request_exchange.toLowerCase();
   const api_key = req.body.api_key;
   const api_secret = req.body.api_secret;
   const api_passphrase = req.body.api_passphrase;
-  const userRef = db.collection('users').doc(userId);
+  const userRef = db.collection("users").doc(userId);
 
   // check if api_passphrase is required
-  if (exchange === 'kucoin' && !api_passphrase) {
-    res.status(400).json({ success: false, error: `api_passphrase is required for ${exchange} exchange` });
+  if (exchange === "kucoin" && !api_passphrase) {
+    res
+      .status(400)
+      .json({ success: false, error: `api_passphrase is required for ${exchange} exchange` });
   } else {
     try {
-
       const updateFields = {
         [`exchanges.${exchange}.api_key`]: api_key,
         [`exchanges.${exchange}.api_secret`]: api_secret,
       };
 
-      if (exchange === 'kucoin') {
+      if (exchange === "kucoin") {
         updateFields[`exchanges.${exchange}.api_passphrase`] = api_passphrase;
       }
 
-      userRef.update(updateFields)
+      userRef
+        .update(updateFields)
         .then(() => {
           console.log(`Exchange information updated successfully - ${userId}!`);
-          res.status(200).json({ success: true, message: `Exchange information updated successfully - ${userId}!` });
+          res.status(200).json({
+            success: true,
+            message: `Exchange information updated successfully - ${userId}!`,
+          });
         })
         .catch((error) => {
           console.error(`Error updating document: ${error}`);
           res.status(500).json({ success: false, error: `Error updating document: ${userId}` });
         });
     } catch (error) {
-      console.error('Error encrypting data:', error);
-      res.status(500).json({ success: false, error: 'An error occurred during the process' });
+      console.error("Error encrypting data:", error);
+      res.status(500).json({ success: false, error: "An error occurred during the process" });
     }
   }
 });
 
-app.post('/updateMargin', async (req, res) => {
-  const userId = req.get("x-forwarded-authorization").split(" ")[1]
+app.post("/updateMargin", async (req, res) => {
+  const userId = req.get("x-forwarded-authorization").split(" ")[1];
 
-  const { license_key, margin, percentage, option, preferred_exchange } = req.body;
+  // license_key can be replaced by product id as its more fitting. Would just need to also adjust
+  // the query snapshot to use product id instead of license key
+  const { license_key, margin, percentage, option, preferred_exchange, worker_id } = req.body;
 
   try {
     // Get user document from Firestore
-    const userDoc = await db.collection('users').doc(userId).get();
+    const userDoc = await db.collection("users").doc(userId).get();
 
     if (!userDoc.exists) {
-      return res.status(404).json({success:false,  error: 'User not found' });
+      return res.status(404).json({ success: false, error: "User not found" });
     }
 
     // Get plan document that matches the license key
     const plansCollectionRef = db.collection(`users/${userId}/plans`);
-    const planQuerySnapshot = await plansCollectionRef.where('license', '==', license_key).get();
+    const planQuerySnapshot = await plansCollectionRef.where("license", "==", license_key).get();
 
     if (planQuerySnapshot.empty) {
-      return res.status(404).json({success:false, error: 'Plan not found for this user and license key' });
+      return res
+        .status(404)
+        .json({ success: false, error: "Plan not found for this user and license key" });
     }
 
-    // Update plan document with new margin, percentage, option, preferred_exchange values
+    // Get worker document
     const planDocRef = planQuerySnapshot.docs[0].ref;
+    const workerDocRef = planDocRef.collection("workers").doc(worker_id);
+
+    // Update worker document with new margin, percentage, option, preferred_exchange values
     const updateFields = {};
 
     if (margin !== "" && margin !== null) {
@@ -94,23 +105,23 @@ app.post('/updateMargin', async (req, res) => {
       updateFields.preferred_exchange = preferred_exchange.toLowerCase();
     }
 
-    await planDocRef.update(updateFields);
+    await workerDocRef.update(updateFields);
 
     res.json({ success: true });
   } catch (error) {
     console.error(error);
-    res.status(500).json({success:false,  error: 'Something went wrong' });
+    res.status(500).json({ success: false, error: "Something went wrong" });
   }
 });
 
-app.get('/exchanges', async (req, res) => {
-  const userId = req.get("x-forwarded-authorization").split(" ")[1]
+app.get("/exchanges", async (req, res) => {
+  const userId = req.get("x-forwarded-authorization").split(" ")[1];
 
   try {
-    const documentSnapshot = await db.collection('users').doc(userId).get();
+    const documentSnapshot = await db.collection("users").doc(userId).get();
 
     if (!documentSnapshot.exists) {
-      res.status(404).json({ success: false, error: 'User not found for exchanges'});
+      res.status(404).json({ success: false, error: "User not found for exchanges" });
       return;
     }
 
@@ -120,7 +131,7 @@ app.get('/exchanges', async (req, res) => {
     const existingApis = {};
     for (const exchange in traderData.exchanges) {
       const { api_key, api_secret } = traderData.exchanges[exchange];
-      if (api_key && api_key !== 'x' && api_secret && api_secret !== 'x') {
+      if (api_key && api_key !== "x" && api_secret && api_secret !== "x") {
         existingApis[exchange] = true;
       } else {
         existingApis[exchange] = false;
@@ -134,33 +145,44 @@ app.get('/exchanges', async (req, res) => {
 
     res.json(responseData);
   } catch (error) {
-    console.log('Error retrieving user data:', error);
-    res.status(500).json({ success: false, error: 'Error retrieving user data' });
+    console.log("Error retrieving user data:", error);
+    res.status(500).json({ success: false, error: "Error retrieving user data" });
   }
 });
 
-app.get('/plans', async (req, res) => {
-  const userId = req.get("x-forwarded-authorization").split(" ")[1]
+app.get("/plans", async (req, res) => {
+  const userId = req.get("x-forwarded-authorization").split(" ")[1];
 
   try {
     // Get the user document
-    const userRef = db.collection('users').doc(userId);
+    const userRef = db.collection("users").doc(userId);
     const userSnapshot = await userRef.get();
 
     if (!userSnapshot.exists) {
-      res.status(404).json({ success: false, error: 'User not found for plans'});
+      res.status(404).json({ success: false, error: "User not found for plans" });
       return;
     }
 
     // Get the plans subcollection
-    const plansRef = userRef.collection('plans');
+    const plansRef = userRef.collection("plans");
     const plansSnapshot = await plansRef.get();
 
     // Extract the data from the plans documents
     const plansData = [];
-    plansSnapshot.forEach((doc) => {
+    plansSnapshot.forEach(async (doc) => {
       if (doc.exists) {
         const planData = doc.data();
+        const workersRef = plansRef.doc(doc.id).collection("workers");
+        const workersSnapshot = await workersRef.get();
+        const workersData = [];
+        workersSnapshot.forEach((workerDoc) => {
+          if (workerDoc.exists) {
+            const workerData = workerDoc.data();
+            workerData.id = workerDoc.id; // Add the worker ID to the data
+            workersData.push(workerData);
+          }
+        });
+        planData.workers = workersData; // Add the workers data to the plan data
         plansData.push(planData);
       }
     });
@@ -170,54 +192,82 @@ app.get('/plans', async (req, res) => {
       plans: plansData,
     };
 
+    // example response data:
+    // {
+    //   "success": true,
+    //   "plans": [
+    //     {
+    //       "product": "{product_id}",
+    //       "product_name": "{product_name}",
+    //       "license": "{license}",
+    //       "account_id": "{account_id}",
+    //       "workers": [
+    //         {
+    //           "id": "{worker_id}",
+    //           "name": "{worker_name}",
+    //           "margin": "{margin}",
+    //           "percentage": "{percentage}",
+    //           "option": "{option}",
+    //           "preferred_exchange": "{preferred_exchange}",
+    //           "enabled": {enabled}
+    //         },
+    //         // ... more workers
+    //       ]
+    //     },
+    //     // ... more plans
+    //   ]
+    // }
+
     res.json(responseData);
   } catch (error) {
-    console.log('Error retrieving plans:', error);
-    res.status(500).json({ success: false, error: 'Error retrieving plans' });
+    console.log("Error retrieving plans:", error);
+    res.status(500).json({ success: false, error: "Error retrieving plans" });
   }
 });
 
-app.get('/pubKey', async (req, res) => {
-    const userId = req.get("x-forwarded-authorization").split(" ")[1]
+app.get("/pubKey", async (req, res) => {
+  const userId = req.get("x-forwarded-authorization").split(" ")[1];
 
-    try {
-        if (!userId) {
-          return res.status(400).json({ success: false, error: 'User name is missing' });
-        }
-
-        const userSnapshot = await db.collection('users').doc(userId).get();
-
-        if (!userSnapshot.exists) {
-          res.status(404).json({ success: false, error: 'User not found for pubKey' });
-          return;
-        }
-
-        const publicKey = await getPublicKey(userId);
-
-        if (publicKey) {
-            res.status(200).json({success: true, publicKey });
-        } else {
-            res.status(404).json({success: false, error: "Public key not found." });
-        }
-    } catch (error) {
-        // Catch any error that occurred while getting the public key
-        console.log(error);
-        res.status(500).json({ success:false, error: "An error occurred while getting the public key." });
+  try {
+    if (!userId) {
+      return res.status(400).json({ success: false, error: "User name is missing" });
     }
+
+    const userSnapshot = await db.collection("users").doc(userId).get();
+
+    if (!userSnapshot.exists) {
+      res.status(404).json({ success: false, error: "User not found for pubKey" });
+      return;
+    }
+
+    const publicKey = await getPublicKey(userId);
+
+    if (publicKey) {
+      res.status(200).json({ success: true, publicKey });
+    } else {
+      res.status(404).json({ success: false, error: "Public key not found." });
+    }
+  } catch (error) {
+    // Catch any error that occurred while getting the public key
+    console.log(error);
+    res
+      .status(500)
+      .json({ success: false, error: "An error occurred while getting the public key." });
+  }
 });
 
 // Second1 Delete:
-app.delete('/deleteExchange', async (req, res) => {
-  const userId = req.get("x-forwarded-authorization").split(" ")[1]
+app.delete("/deleteExchange", async (req, res) => {
+  const userId = req.get("x-forwarded-authorization").split(" ")[1];
 
   const exchangeName = req.body.exchange.toLowerCase();
 
   try {
-    const userRef = db.collection('users').doc(userId);
+    const userRef = db.collection("users").doc(userId);
     const userSnapshot = await userRef.get();
 
     if (!userSnapshot.exists) {
-      res.status(404).json({ success: false, error: 'User not found for delete exchange' });
+      res.status(404).json({ success: false, error: "User not found for delete exchange" });
       return;
     }
 
@@ -225,37 +275,35 @@ app.delete('/deleteExchange', async (req, res) => {
     const exchanges = userData.exchanges;
 
     if (!exchanges.hasOwnProperty(exchangeName)) {
-      res.status(404).json({ success: false, error: 'Exchange not found' });
+      res.status(404).json({ success: false, error: "Exchange not found" });
       return;
     }
 
     // Delete API credentials by setting them to 'x'
-    exchanges[exchangeName].api_key = 'x';
-    exchanges[exchangeName].api_secret = 'x';
+    exchanges[exchangeName].api_key = "x";
+    exchanges[exchangeName].api_secret = "x";
     if (exchanges[exchangeName].api_passphrase) {
-      exchanges[exchangeName].api_passphrase = 'x';
+      exchanges[exchangeName].api_passphrase = "x";
     }
 
     await userRef.update({ exchanges });
 
-    res.json({ success: true, message: 'Exchange credentials deleted successfully' });
+    res.json({ success: true, message: "Exchange credentials deleted successfully" });
   } catch (error) {
-    console.error('Error deleting exchange credentials:', error);
-    res.status(500).json({ success: false, error: 'Internal server error' });
+    console.error("Error deleting exchange credentials:", error);
+    res.status(500).json({ success: false, error: "Internal server error" });
   }
 });
 
-
 app.get("/", (req, res) => {
-    res.send("Hello World");
-})
-
-app.get("*", (req, res) => {
-    return res.status(400).send("Not Authorized");
+  res.send("Hello World");
 });
 
+app.get("*", (req, res) => {
+  return res.status(400).send("Not Authorized");
+});
 
 // expose the express app as a cloud function
 module.exports = {
-    member: app
-}
+  member: app,
+};
