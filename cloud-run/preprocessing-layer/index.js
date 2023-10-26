24921c9a4757efae6206599aa0983b010e67da32
Vantria <s1spam118@gmail.com>
2023-10-26 21:20:38 +0100

    add more logs to pp layer

--- cloud-run/preprocessing-layer/index.js
+++ cloud-run/preprocessing-layer/index.js
@@ -729,8 +729,7 @@ app.post("/bulkOrder", async (req, res) => {
     res.status(200).json({ success: true, message: "Bulk order executed successfully" });
   } catch (error) {
     console.log(`Error executing bulk order: ${error.message}`);
-    console.log("Error code:", error.code);
-    console.log("Error details:", error.details);
+    console.log("Whole error:", error);
     res.status(500).json({ success: false, message: "Internal Server Error" });
   }
 });
