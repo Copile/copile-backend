bf98cc1f6a5d3c0e71979e273412c304cd09d8aa
Jan Tokic <jan.tokic@gmail.com>
2023-11-09 18:28:03 +0100

    - aldin gayness

--- cloud-functions/admintrades/utils/utils.js
+++ cloud-functions/admintrades/utils/utils.js
@@ -1,6 +1,6 @@
 const { Firestore } = require("@google-cloud/firestore");
-const CustomError = require("../../utils/error");
-const createSession = require("../../exchanges/sessionFactory");
+const CustomError = require("./error");
+const createSession = require("../exchanges/sessionFactory");
 
 const db = new Firestore();
 
