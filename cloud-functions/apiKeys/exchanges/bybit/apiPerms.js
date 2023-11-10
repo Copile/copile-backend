696326096864e49c14bde558f0b665cfe0cd274e
Jan Tokic <jan.tokic@gmail.com>
2023-11-10 20:46:39 +0100

    - Added testnet to API key validation

--- cloud-functions/apiKeys/exchanges/bybit/apiPerms.js
+++ cloud-functions/apiKeys/exchanges/bybit/apiPerms.js
@@ -1,19 +1,20 @@
 const { RestClientV5 } = require("bybit-api");
 const CustomError = require("../../utils/error");
 
-async function getBybitAPIPerms(apiKey, apiSecret) {
+async function getBybitAPIPerms(apiKey, apiSecret, isTestnet = false) {
   try {
     const client = new RestClientV5({
       key: apiKey,
       secret: apiSecret,
       strict_param_validation: true,
+      testnet: isTestnet,
     });
     const response = await client.getQueryApiKey();
 
     return response;
   } catch (e) {
     throw new CustomError({
-      message: `Failed to fetch bybit balance: ${e.message}`,
+      message: `Failed to fetch bybit API Perms: ${e.message}`,
       status: 500,
       source: "getBybitAPIPerms",
     });
--- cloud-functions/apiKeys/exchanges/testnet/session.js
+++ cloud-functions/apiKeys/exchanges/testnet/session.js
@@ -1,8 +1,6 @@
 const ExchangeSession = require("../exchangeSession");
 const CustomError = require("../../utils/error");
-const { getTestnetPositions } = require("./positions");
-const { getTestnetOrders, getTestnetOrderStatuses } = require("./orders");
-const { getTestnetBalance } = require("./balance");
+const { getBybitAPIPerms } = require("../bybit/apiPerms");
 
 /**
  * Represents an exchange session for the testnet.
@@ -19,87 +17,22 @@ class TestnetSession extends ExchangeSession {
   }
 
   /**
-   * Fetch orders for a given trader ID.
-   * @async
-   * @param {string} traderId - The unique ID of the trader.
-   * @returns {Promise<Array<Object>>} An array of order data objects.
-   * @throws {CustomError} Throws a custom error if operation fails.
-   */
-  async getOrders(traderId) {
-    try {
-      return await getTestnetOrders(this.apiKey, this.apiSecret, traderId);
-    } catch (e) {
-      if (e instanceof CustomError) {
-        throw e;
-      }
-      throw new CustomError({
-        message: `Failed to fetch testnet orders: ${e.message}`,
-        status: 500,
-        source: "getOrders",
-      });
-    }
-  }
-
-  /**
-   * Fetch positions for a given trader ID.
-   * @async
-   * @param {string} traderId - The unique ID of the trader.
-   * @returns {Promise<Array<Object>>} An array of position data objects.
-   * @throws {CustomError} Throws a custom error if operation fails.
-   */
-  async getPositions(traderId) {
-    try {
-      return await getTestnetPositions(this.apiKey, this.apiSecret, traderId);
-    } catch (e) {
-      if (e instanceof CustomError) {
-        throw e;
-      }
-      throw new CustomError({
-        message: `Failed to fetch testnet positions: ${e.message}`,
-        status: 500,
-        source: "getPositions",
-      });
-    }
-  }
-
-  /**
    * Fetch the balance for a given trader ID.
    * @async
    * @returns {Promise<number>} The trader's balance.
    * @throws {CustomError} Throws a custom error if operation fails.
    */
-  async getBalance() {
+  async getAPIPerms() {
     try {
-      return await getTestnetBalance(this.apiKey, this.apiSecret);
+      return await getBybitAPIPerms(this.apiKey, this.apiSecret, true);
     } catch (e) {
       if (e instanceof CustomError) {
         throw e;
       }
       throw new CustomError({
-        message: `Failed to fetch testnet balance: ${e.message}`,
+        message: `Failed to fetch testnet APIPerms: ${e.message}`,
         status: 500,
-        source: "getBalance",
-      });
-    }
-  }
-
-  /**
-   * Fetch order statuses for a given trader ID.
-   * @async
-   * @returns {Promise<Array<Object>>} An array of order status objects.
-   * @throws {CustomError} Throws a custom error if operation fails.
-   */
-  async getOrderStatuses(symbol) {
-    try {
-      return await getTestnetOrderStatuses(this.apiKey, this.apiSecret, symbol);
-    } catch (e) {
-      if (e instanceof CustomError) {
-        throw e;
-      }
-      throw new CustomError({
-        message: `Failed to fetch testnet order statuses: ${e.message}`,
-        status: 500,
-        source: "getOrderStatuses",
+        source: "getAPIPerms",
       });
     }
   }
