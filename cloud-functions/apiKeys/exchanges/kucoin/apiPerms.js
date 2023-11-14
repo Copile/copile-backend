114f64e9807e4972a798b35e75052eab6d844c37
Jan Tokic <jan.tokic@gmail.com>
2023-11-14 19:47:17 +0100

    - Change KuCoin again

--- cloud-functions/apiKeys/exchanges/kucoin/apiPerms.js
+++ cloud-functions/apiKeys/exchanges/kucoin/apiPerms.js
@@ -18,7 +18,7 @@ async function getKucoinAPIPerms(apiKey, apiSecret, apiPassphrase) {
   try {
     const accountInfoResponse = await futuresSDK.futuresAccount();
     console.log(accountInfoResponse);
-    if (!accountInfoResponse || accountInfoResponse.status !== 200) {
+    if (!accountInfoResponse || accountInfoResponse.code !== "200000") {
       return {
         success: false,
         code: accountInfoResponse?.status || 500,
