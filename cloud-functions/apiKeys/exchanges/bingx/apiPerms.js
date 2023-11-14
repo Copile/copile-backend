a871ce744de06ea427e33c6aa952d2901539ccef
Jan Tokic <jan.tokic@gmail.com>
2023-11-14 21:09:21 +0100

    - Fixed bingX

--- cloud-functions/apiKeys/exchanges/bingx/apiPerms.js
+++ cloud-functions/apiKeys/exchanges/bingx/apiPerms.js
@@ -11,8 +11,7 @@ const { getAPIPerms } = require("./request");
  */
 async function getBingXAPIPerms(apiKey, apiSecret) {
   const response = await getAPIPerms(apiKey, apiSecret);
-  console.log(response.data.code);
-  if (!response || !response.data || !response.data.code === 0) {
+  if (!response || !response.data || response.data.code !== 0) {
     return {
       success: false,
       code: response.data.code,
