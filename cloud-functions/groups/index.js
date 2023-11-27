3730dd0723633a666a3322a5efa9d6d8c6716f6b
Vantria <s1spam118@gmail.com>
2023-11-27 21:10:32 +0000

    add workers functionality to /createPlan

--- cloud-functions/groups/index.js
+++ cloud-functions/groups/index.js
@@ -10,8 +10,34 @@ applyMiddleware(app);
 const WHOP_TOKEN = process.env.whopToken;
 const axios = require("axios");
 
+/**
+ * @summary Creates a new plan and assigns workers to it.
+ *
+ * This endpoint receives a POST request with a body containing the details of a new plan and an array of worker IDs.
+ * It first checks if all the required fields for creating a new plan are present in the request body.
+ * If any required fields are missing, it returns an error.
+ *
+ * It then creates a new plan object with the request body and some default values.
+ * The group ID is temporarily removed from the plan object before it is sent to the Whop API to create the plan.
+ *
+ * After the plan is created, the group ID and plan ID are added back to the plan object,
+ * and the plan is saved in Firestore under the corresponding group.
+ *
+ * If there are any worker IDs provided in the request body, the endpoint fetches each worker's document from the `workers` collection from the group in Firestore.
+ * For each worker document that exists, it adds the worker's data to the `assigned_workers` sub-collection in the plan document.
+ * If a worker document does not exist for a provided ID, it simply skips that ID.
+ *
+ * Once all existing workers have been assigned to the plan, the endpoint sends a success response.
+ *
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next middleware function.
+ */
 app.post("/createPlan", async (req, res, next) => {
+  // Log the start of the plan creation process
   console.log("Creating a new plan...");
+
+  // Define the required fields for creating a new plan
   const requiredFields = [
     "group_id",
     "base_currency",
@@ -23,11 +49,11 @@ app.post("/createPlan", async (req, res, next) => {
     "trial_period_days",
     "unlimited_stock",
   ];
-  console.log("Required fields: ", requiredFields);
 
+  // Check for any missing required fields in the request body
   const missingFields = requiredFields.filter((field) => req.body[field] === undefined);
-  console.log("Missing fields: ", missingFields);
 
+  // If there are missing fields, return an error
   if (missingFields.length) {
     console.log("Error: Missing required fields");
     return next(
@@ -39,7 +65,7 @@ app.post("/createPlan", async (req, res, next) => {
     );
   }
 
-  const groupId = req.body.group_id;
+  // Create a new plan object with the request body and some default values
   const newPlan = {
     ...req.body,
     grace_period_days: 0,
@@ -53,27 +79,60 @@ app.post("/createPlan", async (req, res, next) => {
 
   // Temporarily delete group id before hitting whop. We'll add it back before saving to firestore
   delete newPlan.group_id;
-  console.log("New plan: ", newPlan);
 
   try {
-    console.log("Sending request to create plan...");
+    // Send a request to the Whop API to create the plan
     const { data } = await axios.post("https://api.whop.com/api/v2/plans", newPlan, {
       headers: {
         Authorization: `Bearer ${WHOP_TOKEN}`,
       },
     });
-    console.log("Request sent successfully");
 
-    newPlan.workers = [];
-    newPlan.group_id = groupId;
-    console.log("Adding workers to the new plan...");
+    // Extract the group id and worker ids from the request body and reassign them to variables
+    const { group_id, workers: worker_ids } = req.body;
 
+    // Extract the plan id from the response data
     const plan_id = data.id;
-    await db.collection("groups").doc(groupId).collection("plans").doc(plan_id).set(newPlan);
-    console.log("Workers added successfully");
+
+    // Add the group id back in and plan id to the new plan object
+    newPlan.group_id = group_id;
+    newPlan.plan_id = plan_id;
+
+    // Save the new plan in Firestore under the corresponding group
+    await db.collection("groups").doc(group_id).collection("plans").doc(plan_id).set(newPlan);
+
+    // If there are any workers specified in the request, assign them to the plan in Firestore
+    if (worker_ids && worker_ids.length > 0) {
+      // Get the workers collection and the assigned workers collection in Firestore
+      const workersCollection = db.collection("groups").doc(group_id).collection("workers");
+      const assignedWorkersCollection = db
+        .collection("groups")
+        .doc(group_id)
+        .collection("plans")
+        .doc(plan_id)
+        .collection("assigned_workers");
+
+      // Fetch the worker documents from Firestore
+      const workerSnapshots = await Promise.all(
+        worker_ids.map((id) => workersCollection.doc(id).get())
+      );
+
+      // Filter out any non-existing workers and map the snapshots to their data
+      const existingWorkers = workerSnapshots
+        .filter((snapshot) => snapshot.exists)
+        .map((snapshot) => snapshot.data());
+
+      // Assign the workers to the plan in Firestore
+      await Promise.all(
+        existingWorkers.map((worker) => assignedWorkersCollection.doc(worker.id).set(worker))
+      );
+    }
+
+    // Send a success response
     res.status(200).json({ message: "Plan created successfully" });
   } catch (error) {
     console.log("Failed to create plan:", error.message);
+    // If there's an error, return a custom error
     return next(
       new CustomError({
         message: "Failed to create plan",
