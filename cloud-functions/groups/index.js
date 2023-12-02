8cabb2e23ce10c97317a1e3544258ca08e89d859
Vantria <s1spam118@gmail.com>
2023-12-02 01:11:20 +0000

    add logs to user sync

--- cloud-functions/groups/index.js
+++ cloud-functions/groups/index.js
@@ -363,6 +363,7 @@ app.get("/getData", async (req, res, next) => {
 });
 
 async function findAndSyncUsers(groupId, planId) {
+  console.log(`Starting findAndSyncUsers for groupId: ${groupId} and planId: ${planId}`);
   try {
     // Fetch the plan document
     const groupDocRef = db.collection("groups").doc(groupId);
@@ -378,6 +379,7 @@ async function findAndSyncUsers(groupId, planId) {
     const planWorkersRef = planDocRef.collection("assigned_workers");
     const planWorkersSnapshot = await planWorkersRef.get();
     const planWorkers = planWorkersSnapshot.docs.map((doc) => doc.data());
+    console.log(`Fetched ${planWorkers.length} workers for planId: ${planId}`);
 
     // Fetch all memberships from Whop
     const response = await axios.get(
@@ -388,6 +390,7 @@ async function findAndSyncUsers(groupId, planId) {
         },
       }
     );
+    console.log(`Fetched ${response.data.data.length} memberships for planId: ${planId}`);
 
     // For each membership
     for (const membership of response.data.data) {
@@ -408,12 +411,15 @@ async function findAndSyncUsers(groupId, planId) {
       await new Promise((resolve) => setTimeout(resolve, 1000));
     }
   } catch (error) {
-    console.error(error);
+    console.error(`Error in findAndSyncUsers for groupId: ${groupId} and planId: ${planId}`, error);
   }
 }
 
 const syncUser = async (data, planWorkers) => {
   const { userId, planId, groupId } = data;
+  console.log(
+    `Starting syncUser for userId: ${userId}, planId: ${planId}, and groupId: ${groupId}`
+  );
 
   try {
     // Get the user's plan document reference
@@ -423,6 +429,7 @@ const syncUser = async (data, planWorkers) => {
     const userPlanDoc = await userPlanDocRef.get();
 
     if (!userPlanDoc.exists) {
+      console.log(`User's plan with id: ${planId} for userId: ${userId} not found.`);
       return { success: false, error: "User's plan not found" };
     }
 
@@ -430,12 +437,16 @@ const syncUser = async (data, planWorkers) => {
     const userPlanWorkersRef = userPlanDocRef.collection("workers");
     const userPlanWorkersSnapshot = await userPlanWorkersRef.get();
     const userPlanWorkers = userPlanWorkersSnapshot.docs.map((doc) => doc.data());
+    console.log(
+      `Fetched ${userPlanWorkers.length} workers for userId: ${userId} and planId: ${planId}`
+    );
 
     // For each worker in the plan
     for (const worker of planWorkers) {
       // If the worker is not in the user's plan, add it
       if (!userPlanWorkers.some((userPlanWorker) => userPlanWorker.id === worker.id)) {
         await userPlanWorkersRef.doc(worker.id).set(worker);
+        console.log(`Added worker ${worker.id} to userId: ${userId} and planId: ${planId}`);
       }
     }
 
@@ -444,12 +455,16 @@ const syncUser = async (data, planWorkers) => {
       // If the worker is not in the plan, remove it
       if (!planWorkers.some((planWorker) => planWorker.id === worker.id)) {
         await userPlanWorkersRef.doc(worker.id).delete();
+        console.log(`Removed worker ${worker.id} from userId: ${userId} and planId: ${planId}`);
       }
     }
 
     return { success: true };
   } catch (error) {
-    console.error(error);
+    console.error(
+      `Error in syncUser for userId: ${userId}, planId: ${planId}, and groupId: ${groupId}`,
+      error
+    );
     return { success: false, error: error.message };
   }
 };
