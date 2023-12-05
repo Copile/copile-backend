daee0674433479abf6a347b532aa66b01ff8b631
Vantria <s1spam118@gmail.com>
2023-12-05 23:47:06 +0000

    add logs and adjustments to checkReferral

--- cloud-functions/checkReferral/index.js
+++ cloud-functions/checkReferral/index.js
@@ -12,8 +12,9 @@ app.get("/checkReferral", async (req, res, next) => {
   const referral_code = req.query.code;
   console.log(`referral_code: ${referral_code}`);
 
-  if (!referral_code || typeof referral_code !== "string" || referral_code.trim() === "") {
-    console.log("Missing or invalid required field: referral_code. Sending error response...");
+  if (!referral_code) {
+    console.log("Missing required field: referral_code. Sending error response...");
+    res.status(400).send({ success: false, message: "Missing or invalid required field: referral_code" });
     return next(
       new CustomError({
         message: "Missing or invalid required field: referral_code",
@@ -24,20 +25,25 @@ app.get("/checkReferral", async (req, res, next) => {
   }
 
   try {
+    console.log("Attempting to fetch referral code from database...");
     const plansRef = db.collectionGroup("plans");
+    console.log("plansRef: ", plansRef);
     const snapshot = await plansRef.where("referral_code", "==", referral_code).get();
+    console.log("Snapshot: ", snapshot);
 
     if (snapshot.empty) {
-      console.log("No matching documents.");
-      return res.send({ success: false });
+      console.log("No matching documents. Sending error response...");
+      return res.send({ success: false, message: "No matching referral." });
     }
 
     const doc = snapshot.docs[0];
-    console.log(doc.id, "=>", doc.data());
+    console.log(`Found matching document: ${doc.id} => ${JSON.stringify(doc.data())}`);
     const data = doc.data();
+    console.log("Sending success response with direct link...");
     res.send({ success: true, direct_link: data.direct_link });
   } catch (error) {
     console.error("Error occurred while fetching referral code: ", error);
+    res.status(500).send({ success: false, message: "Failed to check referral code" });
     return next(
       new CustomError({
         message: "Failed to check referral code",
