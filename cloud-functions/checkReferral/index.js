2e31fda556cbc3994351347a2b5b758322c68385
Vantria <s1spam118@gmail.com>
2023-12-06 01:46:03 +0000

    add adjustments to checkReferral

--- cloud-functions/checkReferral/index.js
+++ cloud-functions/checkReferral/index.js
@@ -1,7 +1,7 @@
 const { Firestore } = require("@google-cloud/firestore");
-const firestore = new Firestore();
+const db = new Firestore();
 const express = require("express");
-const CustomError = require("./utils/CustomError");
+const CustomError = require("./utils/error");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 
@@ -27,7 +27,7 @@ app.get("/checkReferral", async (req, res, next) => {
 
   try {
     console.log("Attempting to fetch referral code from database...");
-    const plansRef = firestore.collectionGroup("plans");
+    const plansRef = db.collectionGroup("plans");
     console.log("plansRef: ", plansRef);
     const snapshot = await plansRef.where("referral_code", "==", referral_code).get();
     console.log("Snapshot: ", snapshot);
