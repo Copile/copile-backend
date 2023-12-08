73a5a5abf5c6229d58d09be3d9d31e285f5d323a
ALADIN847 <aldinmalic12@gmail.com>
2023-12-08 18:48:57 +0100

    Error handling + comments for kucoin

--- cloud-functions/submitTrade-py/exchanges/kucoin/api/request.py
+++ cloud-functions/submitTrade-py/exchanges/kucoin/api/request.py
@@ -5,6 +5,9 @@ import hmac
 import json
 import base64
 from urllib.parse import urlencode
+import logging
+
+logger = logging.getLogger(__name__)
 
 api_config = {
     "host": "api-futures.kucoin.com",
@@ -13,40 +16,46 @@ api_config = {
 
 
 def sign_request(str_to_sign, api_secret):
-    signature = base64.b64encode(
-        hmac.new(api_secret.encode('utf-8'), str_to_sign.encode('utf-8'), hashlib.sha256).digest()
-    ).decode('utf-8')
-    return signature
+    try:
+        signature = base64.b64encode(
+            hmac.new(api_secret.encode('utf-8'), str_to_sign.encode('utf-8'), hashlib.sha256).digest()
+        ).decode('utf-8')
+        return signature
+    except Exception as e:
+        logger.error("An error occurred while creating signature: %s", e, exc_info=True)
 
 
 async def make_signed_request(method, path, payload, api_key, api_secret, api_passphrase):
-    timestamp = str(int(time.time() * 1000))
+    try:
+        timestamp = str(int(time.time() * 1000))
 
-    headers = {
-        "Content-Type": "application/json",
-        "KC-API-KEY": api_key,
-        "KC-API-KEY-VERSION": "2",
-        "KC-API-TIMESTAMP": timestamp
-    }
+        headers = {
+            "Content-Type": "application/json",
+            "KC-API-KEY": api_key,
+            "KC-API-KEY-VERSION": "2",
+            "KC-API-TIMESTAMP": timestamp
+        }
 
-    if method in ['GET', 'DELETE']:
-        query_string = urlencode(payload) if payload else ''
-        str_to_sign = f'{timestamp}{method}{path}?{query_string}'
-        url = f"{api_config['protocol']}://{api_config['host']}{path}?{query_string}"
-    else:
-        str_to_sign = f'{timestamp}{method}{path}{json.dumps(payload)}'
-        url = f"{api_config['protocol']}://{api_config['host']}{path}"
+        if method in ['GET', 'DELETE']:
+            query_string = urlencode(payload) if payload else ''
+            str_to_sign = f'{timestamp}{method}{path}?{query_string}'
+            url = f"{api_config['protocol']}://{api_config['host']}{path}?{query_string}"
+        else:
+            str_to_sign = f'{timestamp}{method}{path}{json.dumps(payload)}'
+            url = f"{api_config['protocol']}://{api_config['host']}{path}"
 
-    signature = sign_request(str_to_sign, api_secret)
-    headers['KC-API-SIGN'] = signature
-    passphrase = base64.b64encode(
-        hmac.new(api_secret.encode('utf-8'), api_passphrase.encode('utf-8'), hashlib.sha256).digest()
-    ).decode('utf-8')
-    headers['KC-API-PASSPHRASE'] = passphrase
-    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=True)) as session:
-        async with session.request(method, url, headers=headers,
-                                   json=payload if method not in ['GET', 'DELETE'] else None) as response:
-            if response.status != 200:
-                raise Exception(f"Failed to send Kucoin API request to {path}: {await response.json()}")
-            response_json = await response.json()
-            return response_json['data']
+        signature = sign_request(str_to_sign, api_secret)
+        headers['KC-API-SIGN'] = signature
+        passphrase = base64.b64encode(
+            hmac.new(api_secret.encode('utf-8'), api_passphrase.encode('utf-8'), hashlib.sha256).digest()
+        ).decode('utf-8')
+        headers['KC-API-PASSPHRASE'] = passphrase
+        async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=True)) as session:
+            async with session.request(method, url, headers=headers,
+                                    json=payload if method not in ['GET', 'DELETE'] else None) as response:
+                if response.status != 200:
+                    raise Exception(f"Failed to send Kucoin API request to {path}: {await response.json()}")
+                response_json = await response.json()
+                return response_json['data']
+    except Exception as e:
+        logger.error("An error occurred while sending request: %s", e, exc_info=True)
--- cloud-functions/submitTrade-py/exchanges/kucoin/execution.py
+++ cloud-functions/submitTrade-py/exchanges/kucoin/execution.py
@@ -15,6 +15,7 @@ logger = logging.getLogger(__name__)
 
 async def bulk_order(api_key, api_secret, api_passphrase, data):
     try:
+        # Creating session for kucoin api
         session = KucoinFunctions(api_key, api_secret, api_passphrase)
 
         traderId = data['traderId']
@@ -23,7 +24,9 @@ async def bulk_order(api_key, api_secret, api_passphrase, data):
         trader_exchange = data['trader_exchange']
         # margin_type = "ISOLATED-MARGIN" if data['margin_type'] == "ISOLATED" else "REGULAR_MARGIN"
 
+        # Reformatting symbol name for kucoin style
         symbol = reformat_symbol(data['payload']['symbol'])
+        
         side = data['payload']['side'].lower()
         leverage = data['payload']['leverage']
         entry = data['payload']['entry']
@@ -32,15 +35,16 @@ async def bulk_order(api_key, api_secret, api_passphrase, data):
 
         order_type = "limit" if entry != "market" else "market"
 
+        # Fetching precision for specific symbol
         precision = await session.get_precisions(symbol)
-        print(precision)
+
         if entry != "market":
             # Calculate quantity when a specific price is provided
             adjusted_margin = float(margin) * int(leverage)
             quantity_value = round(adjusted_margin / float(entry), precision['quantity_precision'])
             quantity = int(quantity_value / precision['multiplier'])
         else:
-            # Calculate quantity when the price is 'market'
+            # Calculate quantity when the entry is market
             market_price = await session.get_market(symbol)
             adjusted_margin = float(margin) * int(leverage)
             quantity_value = round(adjusted_margin / market_price, precision['quantity_precision'])
@@ -49,20 +53,26 @@ async def bulk_order(api_key, api_secret, api_passphrase, data):
         initial_order = Order(symbol, order_type, side, entry if entry != 'market' else None, quantity, leverage, None,
                               None, None, None)
 
+        # Adding all orders to an array for execution
         prepared_orders = [initial_order]
 
+        # Calculating new take-profits for trade
         new_take_profits = await calculate_tp_amounts(take_profits, quantity, precision)
-        print(new_take_profits)
+
+        # Preparing position sides for take-profits and stop-losses
         tp_sl_side = "sell" if side == "buy" else "buy"
         stop_sl = "up" if side == "sell" else "down"
         stop_tp = "up" if side == "buy" else "down"
 
+
+        # Preparing/Adding take-profits to orders array
         for tp in new_take_profits:
             tp_price = round(float(tp['tp_value']), precision["price_precision"])
             tp_order = Order(symbol, "market", tp_sl_side, tp_price, tp['tp_amount'], leverage, stop_tp, "MP", tp_price,
                              True)
             prepared_orders.append(tp_order)
 
+        # Preparing/Adding stop-losses to orders array
         for sl in stop_losses:
             sl_price = round(float(sl['sl_value']), precision["price_precision"])
             sl['sl_amount'] = round(float(quantity) * float(sl['sl_percentage']), precision["quantity_precision"])
@@ -70,6 +80,7 @@ async def bulk_order(api_key, api_secret, api_passphrase, data):
                              True)
             prepared_orders.append(sl_order)
 
+        # Executing all orders in the orders array 
         order_ids = await asyncio.gather(*(session.trade_order(order) for order in prepared_orders))
 
         new_take_profits_with_ids = []
@@ -102,6 +113,7 @@ async def bulk_order(api_key, api_secret, api_passphrase, data):
             stop_losses_with_ids.append(sl_order_dict)
             sl_count += 1
 
+        # Preparing trade info for storing in firestore
         trade_info = {
             "trade_id": tradeId,
             "order_id": order_ids[0]["orderId"],
@@ -115,8 +127,10 @@ async def bulk_order(api_key, api_secret, api_passphrase, data):
             "exchange": trader_exchange
         }
 
+        # Storing trade info in firestore
         await store_trade(traderId, trade_info)
 
+        # Storing take-profits and stop-losses in firestore
         tp_promises = [store_tp(traderId, tp) for tp in new_take_profits_with_ids]
         sl_promises = [store_sl(traderId, sl) for sl in stop_losses_with_ids]
 
@@ -130,6 +144,7 @@ async def bulk_order(api_key, api_secret, api_passphrase, data):
 
 async def send_sl(api_key, api_secret, api_passphrase, data):
     try:
+        # Creating session for kucoin api
         session = KucoinFunctions(api_key, api_secret, api_passphrase)
 
         traderId = data['traderId']
@@ -137,33 +152,41 @@ async def send_sl(api_key, api_secret, api_passphrase, data):
         document_id = data['sl_id']
         payload = data['payload']
 
+        # Fetching the trade info from firestore
         trade_info = await get_trade_info(traderId, tradeId)
-
         symbol = trade_info["symbol"]
         side = trade_info["side"]
         leverage = trade_info["leverage"]
+
+        # Preparing position side stop-loss        
         sl_side = "sell" if side == "buy" else "buy"
         stop = "up" if side == "sell" else "down"
 
+        # Fetching current position and precision of symbol
         precision, position = await asyncio.gather(
             session.get_precisions(symbol),
             session.get_position(symbol)
         )
 
+        # Getting current position quantity to use for stop-loss order
         position_quantity = get_position_quantity(position, trade_info)
 
         price = round(float(payload["sl_value"]), precision["price_precision"])
 
+        # Creating stop-loss order object
         order = Order(symbol, "market", sl_side, price, position_quantity, leverage, stop, "MP", price,
                       True)
 
+        # Executing new stop-loss order
         create_order = await session.trade_order(order)
 
+        # Preparing payload for storing in firestore
         payload['trade_id'] = tradeId
         payload["sl_amount"] = position_quantity
         payload["order_id"] = create_order["orderId"]
         payload['sl_document_id'] = document_id
 
+        # Storing stop-loss in firestore
         await store_sl(traderId, payload)
 
         return
@@ -174,6 +197,7 @@ async def send_sl(api_key, api_secret, api_passphrase, data):
 
 async def replace_sl(api_key, api_secret, api_passphrase, data):
     try:
+        # Creating session for kucoin api
         session = KucoinFunctions(api_key, api_secret, api_passphrase)
 
         traderId = data['traderId']
@@ -181,33 +205,43 @@ async def replace_sl(api_key, api_secret, api_passphrase, data):
         document_id = data['document_id']
         payload = data['payload']
 
+        # Fetching the trade info from firestore
         trade_info = await get_trade_info(traderId, tradeId)
         symbol = trade_info["symbol"]
         side = trade_info["side"]
         leverage = trade_info["leverage"]
+
+        # Preparing position side stop-loss
         sl_side = "sell" if side == "buy" else "buy"
         stop = "up" if side == "sell" else "down"
 
+        # Fetching current position and precision of symbol
+        # Cancelling old stop-loss
         precision, position, cancel = await asyncio.gather(
             session.get_precisions(symbol),
             session.get_position(symbol),
             send_cancel(session, symbol, traderId, tradeId, document_id, "sl")
         )
 
+        # Getting current position quantity to use for stop-loss order
         position_quantity = get_position_quantity(position, trade_info)
 
         price = round(float(payload["sl_value"]), precision["price_precision"])
 
+        # Creating stop-loss order object
         order = Order(symbol, "market", sl_side, price, position_quantity, leverage, stop, "MP", price,
                       True)
 
+        # Executing new stop-loss order
         create_order = await session.trade_order(order)
 
+        # Preparing payload for storing in firestore
         payload['trade_id'] = tradeId
         payload["sl_amount"] = position_quantity
         payload["order_id"] = create_order["orderId"]
         payload['sl_document_id'] = document_id
 
+        # Storing stop-loss in firestore
         await store_sl(traderId, payload)
 
         return
@@ -218,6 +252,7 @@ async def replace_sl(api_key, api_secret, api_passphrase, data):
 
 async def cancel_order(api_key, api_secret, api_passphrase, data):
     try:
+        # Creating session for kucoin api
         session = KucoinFunctions(api_key, api_secret, api_passphrase)
 
         traderId = data['traderId']
@@ -239,26 +274,33 @@ async def cancel_order(api_key, api_secret, api_passphrase, data):
 
 async def cancel_all_orders(api_key, api_secret, api_passphrase, data):
     try:
+        # Creating session for kucoin api
         session = KucoinFunctions(api_key, api_secret, api_passphrase)
 
         traderId = data['traderId']
         tradeId = data['tradeId']
 
+        # Fetching the trade info from firestore
         trade_info = await get_trade_info(traderId, tradeId)
-
         symbol = trade_info["symbol"]
 
+        # Getting current position info
         position = await session.get_position(symbol)
 
+        # Getting current position quantity to use for cancel order
         quantity = get_position_quantity(position, trade_info)
 
         if quantity != 0:
             side = trade_info['side']
             leverage = trade_info['leverage']
 
+            # Creating order object for selling whole order
             order = Order(symbol, "market", side, None, quantity, leverage, None, None, None, True)
+            
+            # Executing sell order to stop trade
             await session.trade_order(order)
         else:
+            # Cancelling existing limit order
             await session.cancel_order(trade_info['orderID'])
 
         await session.cancel_all_orders(symbol)
@@ -271,16 +313,18 @@ async def cancel_all_orders(api_key, api_secret, api_passphrase, data):
 
 async def cancel_all_tps(api_key, api_secret, api_passphrase, data):
     try:
+        # Creating session for kucoin api
         session = KucoinFunctions(api_key, api_secret, api_passphrase)
 
         traderId = data['traderId']
         tradeId = data['tradeId']
 
-        trade_info, tp_orders = await asyncio.gather(
-            get_trade_info(traderId, tradeId),
+        # Fetching the current take-profit orders from firestore
+        tp_orders = await asyncio.gather(
             get_tp_orders(traderId, tradeId)
         )
 
+        # Cancelling all current take-profits order and deleting them from firestore
         await asyncio.gather(
             *[session.cancel_order(order['orderID']) for order in tp_orders])
         await asyncio.gather(
@@ -293,17 +337,20 @@ async def cancel_all_tps(api_key, api_secret, api_passphrase, data):
 
 async def bulk_tp(api_key, api_secret, api_passphrase, data):
     try:
+        # Creating session for kucoin api
         session = KucoinFunctions(api_key, api_secret, api_passphrase)
 
         traderId = data['traderId']
         tradeId = data['tradeId']
         take_profits = data['take_profits']
 
+        # Fetching the trade info from firestore
         trade_info = await get_trade_info(traderId, tradeId)
-
         symbol = trade_info["symbol"]
         side = trade_info["side"]
         leverage = trade_info["leverage"]
+
+        # Preparing position sides for take-profits
         tp_side = "sell" if side == "buy" else "buy"
         stop = "up" if side == "sell" else "down"
 
@@ -355,12 +402,14 @@ async def bulk_tp(api_key, api_secret, api_passphrase, data):
 
 async def partial_close(api_key, api_secret, api_passphrase, data):
     try:
+        # Creating session for kucoin api
         session = KucoinFunctions(api_key, api_secret, api_passphrase)
 
         traderId = data['traderId']
         tradeId = data['tradeId']
         percentage = data['percentage']
 
+        # Fetching the trade info and current take-profits/stop-losses from firestore
         trade_info, tp_sl_orders = await asyncio.gather(
             get_trade_info(traderId, tradeId),
             get_tp_sl_orders(traderId, tradeId)
@@ -369,6 +418,8 @@ async def partial_close(api_key, api_secret, api_passphrase, data):
         symbol = trade_info["symbol"]
         side = trade_info["side"]
         leverage = trade_info["leverage"]
+        
+        # Preparing position sides for take-profits and stop-losses
         tp_sl_side = "sell" if side == "buy" else "buy"
         stop = "up" if side == "sell" else "down"
 
@@ -382,24 +433,35 @@ async def partial_close(api_key, api_secret, api_passphrase, data):
             session.get_precisions(symbol)
         )
 
+        # Variable for determining if trade executed or still limit
         executed = True if float(position['positionAmt']) != 0 else False
 
+        # Fetching the current position quantity for further use
         position_quantity = get_position_quantity(position, trade_info)
 
         quantity_to_sell = round(position_quantity * float(percentage), precision["quantity_precision"])
         new_quantity = round(position_quantity - quantity_to_sell, precision["quantity_precision"])
 
+        # Fetching the statuses of all take-profits for filtering active/inactive
         tps_data = await get_tps_status(session, tp_orders, trade_info)
 
         take_profits = distribute_percentages(tps_data)
 
+        # Calcuting new take-profits for replacing current ones
         new_take_profits = await calculate_tp_amounts(take_profits, new_quantity, precision)
 
+        # Cancelling all active tps/sls as well as old limit orders
         await session.cancel_all_orders(symbol)
 
+
         if executed:
+            # Creating sell order object for selling partial quantity
             sell_order = Order(symbol, "market", side, None, quantity_to_sell, leverage, None, None, None, True)
+            
+            # Executing sell order to decrease quantity
             await session.trade_order(sell_order)
+            
+            # Updating new quantity in firestore
             await update_trade_quantity(traderId, tradeId, new_quantity)
         else:
             order = Order(symbol, "limit", side, trade_info['entry'], new_quantity, leverage, None, None, None, False)
