3b9877e6926baa029e0fca406e5f83149ae2dfcc
ALADIN847 <aldinmalic12@gmail.com>
2023-12-17 00:52:39 +0100

    Updated logging structure for bingx

--- cloud-run/new-versions/BINGX/utils/decryption.py
+++ cloud-run/new-versions/BINGX/utils/decryption.py
@@ -1,11 +1,10 @@
 import base64
 import logging
 from google.cloud import kms_v1
-from google.protobuf.json_format import MessageToDict
 
 logger = logging.getLogger(__name__)
 
-async def decrypt_data(account_id, ciphertext):
+async def decrypt_data(trader_id, ciphertext):
     try:
         # Create the client.
         client = kms_v1.KeyManagementServiceClient()
@@ -13,16 +12,12 @@ async def decrypt_data(account_id, ciphertext):
         # Convert the ciphertext to bytes.
         ciphertext_bytes = base64.b64decode(ciphertext)
 
-        try:
-            # Use the KMS API to decrypt the data.
-            response = client.asymmetric_decrypt(request={'name': f"projects/copile/locations/global/keyRings/UserAPIKeys/cryptoKeys/{account_id}/cryptoKeyVersions/1", 'ciphertext': ciphertext_bytes})
+        # Use the KMS API to decrypt the data.
+        response = client.asymmetric_decrypt(request={'name': f"projects/copile/locations/global/keyRings/UserAPIKeys/cryptoKeys/{trader_id}/cryptoKeyVersions/1", 'ciphertext': ciphertext_bytes})
 
-            # Extract and return the plaintext.
-            plaintext = response.plaintext
-            return plaintext.decode('utf-8')
-
-        except Exception as e:
-            print(f'Error decrypting data: {e}')
-            raise e
+        # Extract and return the plaintext.
+        plaintext = response.plaintext
+        return plaintext.decode('utf-8')
     except Exception as e:
         logger.error("An error occurred: %s", e, exc_info=True)
+        raise e
