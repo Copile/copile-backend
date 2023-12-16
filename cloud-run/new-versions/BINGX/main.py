02e29b597454b340023e1a89b1c292be7659d764
ALADIN847 <aldinmalic12@gmail.com>
2023-12-16 23:28:29 +0100

    Small shti to get commit

--- cloud-run/new-versions/BINGX/main.py
+++ cloud-run/new-versions/BINGX/main.py
@@ -6,12 +6,6 @@ from .bingx_api.api.session import BingXSession
 
 app = FastAPI()
 
-
-@app.get('/test')
-async def test():
-    return {"message": "Hello World"}
-
-
 @app.post('/submit_sl')
 async def submit_sl(data: dict, traderId: str = Header(None)):
     try:
