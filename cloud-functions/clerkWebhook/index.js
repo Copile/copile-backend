8864d7bc7bb0b0550035313b49c6eb6d3a49984f
Vantria <s1spam118@gmail.com>
2023-12-18 02:24:45 +0000

    add fix to delay for addworker

--- cloud-functions/clerkWebhook/index.js
+++ cloud-functions/clerkWebhook/index.js
@@ -120,7 +120,7 @@ app.post("/addWorkerToGroup", async (req, res) => {
         try {
           await workersRef.doc(organizationMembership.data.public_user_data.user_id).set({
             id: organizationMembership.data.public_user_data.user_id,
-            email: organizationMembership.data.public_user_data.identifier,
+            email: email,
             name: name,
           });
           console.log("Worker added to group");
@@ -137,7 +137,7 @@ app.post("/addWorkerToGroup", async (req, res) => {
     try {
       await workersRef.doc(organizationMembership.data.public_user_data.user_id).set({
         id: organizationMembership.data.public_user_data.user_id,
-        email: organizationMembership.data.public_user_data.identifier,
+        email: email,
         name: name,
       });
       console.log("Worker added to group");
