4b3c5287a7e6c0ff57aa903d8ad6efc42c510c7e
Vantria <s1spam118@gmail.com>
2023-12-19 18:31:51 +0000

    add logs to api validation

--- cloud-functions/apiKeys/routes.js
+++ cloud-functions/apiKeys/routes.js
@@ -17,35 +17,47 @@ router.post("/validate/:exchange", validateEntity, async (req, res, next) => {
   var apiPassphrase = req.body.api_passphrase || null;
 
   try {
+    console.log(`Starting validation for exchange: ${exchange}`);
     if (!apiKey || !apiSecret) {
+      console.log("API credentials are missing.");
       throw new CustomError({
         message: "API credentials are required.",
         status: 400,
         source: "validateAPIKeys",
       });
     }
+    console.log("Decrypting API Secret...");
     apiSecret = await decryptData(apiSecret, entityId);
+    console.log("Decrypted API Secret successfully.", apiSecret);
 
     if (apiPassphrase) {
+      console.log("Decrypting API Passphrase...");
       apiPassphrase = await decryptData(apiPassphrase, entityId);
+      console.log("Decrypted API Passphrase successfully.", apiPassphrase);
     }
 
     if (exchange === "kucoin" && apiPassphrase === null) {
+      console.log("Kucoin requires a passphrase but it's missing.");
       throw new CustomError({
         message: "Kucoin requires a passphrase",
         status: 400,
         source: "validateAPIKeys",
       });
     }
+    console.log("Creating session...");
     const session = createSession(exchange, apiKey, apiSecret, apiPassphrase);
+    console.log("Fetching API Permissions...");
     const apiPermsResponse = await session.getAPIPerms();
+    console.log("Api permissions response: ", apiPermsResponse);
 
+    console.log("API Permissions fetched successfully.");
     return res.status(200).json(apiPermsResponse);
   } catch (e) {
+    console.log("An error occurred: ", e);
     if (e instanceof CustomError) {
       next(e);
     } else {
-      console.log(e);
+      console.log("An error occurred while fetching the API Key Permissions.", e);
       next(
         new CustomError({
           message: "An error occurred while fetching the API Key Permissions.",
