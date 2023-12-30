971fb5cf8b598e1bd85bb7ae43e7be66cbc00101
Vantria <s1spam118@gmail.com>
2023-12-30 15:26:28 +0000

    adjustments to checkReferral

--- cloud-functions/checkReferral/index.js
+++ cloud-functions/checkReferral/index.js
@@ -15,9 +15,7 @@ app.get("/checkReferral", async (req, res, next) => {
 
   if (!referral_code) {
     console.log("Missing required field: referral_code. Sending error response...");
-    return res
-      .status(400)
-      .send({ success: false, message: "Missing or invalid required field: referral_code" });
+    return res.status(400).send({ success: false, message: "Missing or invalid required field: referral_code" });
     // return next(
     //   new CustomError({
     //     message: "Missing or invalid required field: referral_code",
@@ -44,6 +42,55 @@ app.get("/checkReferral", async (req, res, next) => {
     const data = doc.data();
     console.log("Sending success response with direct link...");
     return res.send({ success: true, direct_link: data.direct_link });
+  } catch (error) {
+    console.error("Error occurred while fetching referral code: ", error);
+    return res.status(500).send({ success: false, message: "Failed to check referral code" });
+    // return next(
+    //   new CustomError({
+    //     message: "Failed to check referral code",
+    //     status: 500,
+    //     source: "checkReferralCode",
+    //   })
+    // );
+  }
+});
+
+app.get("/plan", async (req, res, next) => {
+  console.log("=====================================");
+
+  console.log("plan endpoint hit. Processing request...");
+  const referral_code = req.query.code;
+  console.log(`referral_code: ${referral_code}`);
+
+  if (!referral_code) {
+    console.log("Missing required field: referral_code. Sending error response...");
+    return res.status(400).send({ success: false, message: "Missing or invalid required field: referral_code" });
+    // return next(
+    //   new CustomError({
+    //     message: "Missing or invalid required field: referral_code",
+    //     status: 400,
+    //     source: "checkReferralCode",
+    //   })
+    // );
+  }
+
+  try {
+    console.log("Attempting to fetch referral code from database...");
+    const plansRef = db.collectionGroup("plans");
+    console.log("plansRef: ", plansRef);
+    const snapshot = await plansRef.where("referral_code", "==", referral_code).get();
+    console.log("Snapshot: ", snapshot);
+
+    if (snapshot.empty) {
+      console.log("No matching documents. Sending error response...");
+      return res.send({ success: false, message: "No matching referral." });
+    }
+
+    const doc = snapshot.docs[0];
+    console.log(`Found matching document: ${doc.id} => ${JSON.stringify(doc.data())}`);
+    const data = doc.data();
+    console.log("Sending success response with direct link...");
+    return res.send({ success: true, plan: data });
   } catch (error) {
     console.error("Error occurred while fetching referral code: ", error);
     return res.status(500).send({ success: false, message: "Failed to check referral code" });
