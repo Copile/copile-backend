ac25691258cbf950093f3cf32d31ebefedef0514
Vantria <s1spam118@gmail.com>
2024-01-14 03:29:08 +0000

    add binance eu cloud func to avoid geolocation restriction on fetching symbol list

---
+++ cloud-functions/binance/index.js
@@ -0,0 +1,44 @@
+const Firestore = require("@google-cloud/firestore");
+const db = new Firestore();
+const express = require("express");
+const applyMiddleware = require("./middleware");
+const app = express();
+applyMiddleware(app);
+const axios = require("axios");
+
+// Second endpoint: Forward traffic to "https://fapi.binance.com/fapi/v1/exchangeInfo?permissions=PERPETUAL"
+app.get("/supportedExchanges", async (req, res) => {
+  const trader = req.get("traderId");
+
+  if (!trader) {
+    return res.status(400).json({ success: false, error: "Trader ID is missing" });
+  }
+
+  const traderRef = db.collection("traders").doc(trader);
+  const traderDoc = await traderRef.get();
+
+  if (!traderDoc.exists) {
+    return res.status(404).json({ success: false, error: "Trader not found" });
+  }
+
+  const url = "https://fapi.binance.com/fapi/v1/exchangeInfo?permissions=PERPETUAL";
+
+  try {
+    const response = await axios.get(url);
+    return res.json(response.data);
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error: "Failed to fetch data from the external API",
+    });
+  }
+});
+
+app.get("*", (req, res) => {
+  return res.status(400).send("Not Authorized");
+});
+
+// expose the express app as a cloud function
+module.exports = {
+  balances: app,
+};
