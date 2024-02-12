411a9c1ad36567eda918b30031a8efd1aa097776
ALADIN847 <aldinmalic12@gmail.com>
2024-02-12 18:55:20 +0100

    tested metatrader connection

--- cloud-run/exec-handlers/METATRADER5/mt5-api/execution/bulk_order.py
+++ cloud-run/exec-handlers/METATRADER5/mt5-api/execution/bulk_order.py
@@ -1,18 +1,22 @@
 import time
 import asyncio
 import MetaTrader5 as mt5
-from ..scripts.order_factory import Order
+#from ..scripts.order_factory import Order
 
 async def bulk_order(login_id, password, server):
     try:
         # Create server connection to mt5
-        mt5.initiliaze(login=login_id, server=server, password=password)
+        mt5.initialize(login=login_id, server=server, password=password)
+
+        symbol = "USDJPY"
+        symbol_info = mt5.symbol_info(symbol)
 
         print(mt5.terminal_info())
         print(mt5.version())
 
-
+        mt5.shutdown()
 
     except Exception as e:
         print(e)
 
+asyncio.run(bulk_order(79099544, "A-RtFf7b", "MetaQuotes-Demo"))
