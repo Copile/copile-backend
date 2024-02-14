e99101bf78a47f780fe47f6945c7b883ed0612f8
Vantria <s1spam118@gmail.com>
2024-02-14 20:19:56 +0000

    Refactor fetching of meta accounts and plans again

--- cloud-functions/trader/index.js
+++ cloud-functions/trader/index.js
@@ -439,12 +439,16 @@ app.get("/account", async (req, res) => {
     //   metaAccounts.push({ login_id, server, nickname });
     // }
 
-    const metaAccountsCollectionSnapshot = await db;
-    traderData.collection("accounts").collection("meta_accounts").get();
+    const metaAccountsCollectionSnapshot = await db
+      .collection("traders")
+      .doc(traderId)
+      .collection("accounts")
+      .collection("meta_accounts")
+      .get();
     const metaAccounts = metaAccountsCollectionSnapshot.docs.map((doc) => doc.data());
 
     // Fetch product IDs from the 'plans' subcollection
-    const plansCollectionSnapshot = await traderData.collection("plans").get();
+    const plansCollectionSnapshot = await db.collection("traders").doc(traderId).collection("plans").get();
     const plans = plansCollectionSnapshot.docs.map((doc) => doc.data()); // fetch document data
     const responseData = {
       success: true,
