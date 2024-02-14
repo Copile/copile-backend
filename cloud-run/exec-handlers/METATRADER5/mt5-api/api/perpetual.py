7273fb93d402442ed3445b0be5f492bf9c46ad21
ALADIN847 <aldinmalic12@gmail.com>
2024-02-14 20:25:12 +0100

    Fixed perpetual for metatrader

--- cloud-run/exec-handlers/METATRADER5/mt5-api/api/perpetual.py
+++ cloud-run/exec-handlers/METATRADER5/mt5-api/api/perpetual.py
@@ -1,3 +1,4 @@
+import datetime
 import MetaTrader5 as mt5
 
 # Function to place a trade on MT5
@@ -30,34 +31,34 @@ def place_order(order_type, symbol, volume, stop_loss, take_profit, comment, dir
         request['type'] = mt5.ORDER_TYPE_SELL_STOP
         request['action'] = mt5.TRADE_ACTION_PENDING
         if price <= 0:
-            raise exceptions.MetaTraderIncorrectStopPriceError
+            print("Incorrect StopPrice")
         else:
             request['price'] = round(price, 3)
-            request['type_filling'] = MetaTrader5.ORDER_FILLING_RETURN
+            request['type_filling'] = mt5.ORDER_FILLING_RETURN
     elif order_type == "BUY_STOP":
-        request['type'] = MetaTrader5.ORDER_TYPE_BUY_STOP
-        request['action'] = MetaTrader5.TRADE_ACTION_PENDING
+        request['type'] = mt5.ORDER_TYPE_BUY_STOP
+        request['action'] = mt5.TRADE_ACTION_PENDING
         if price <= 0:
-            raise exceptions.MetaTraderIncorrectStopPriceError
+            print("Incorrect StopPrice")
         else:
             request['price'] = round(price, 3)
-            request['type_filling'] = MetaTrader5.ORDER_FILLING_RETURN
+            request['type_filling'] = mt5.ORDER_FILLING_RETURN
 
     elif order_type == "SELL":
-        request['type'] = MetaTrader5.ORDER_TYPE_SELL
-        request['action'] = MetaTrader5.TRADE_ACTION_DEAL
-        request['type_filling'] = MetaTrader5.ORDER_FILLING_IOC
+        request['type'] = mt5.ORDER_TYPE_SELL
+        request['action'] = mt5.TRADE_ACTION_DEAL
+        request['type_filling'] = mt5.ORDER_FILLING_IOC
     elif order_type == "BUY":
-        request['type'] = MetaTrader5.ORDER_TYPE_BUY
-        request['action'] = MetaTrader5.TRADE_ACTION_DEAL
-        request['type_filling'] = MetaTrader5.ORDER_FILLING_IOC
+        request['type'] = mt5.ORDER_TYPE_BUY
+        request['action'] = mt5.TRADE_ACTION_DEAL
+        request['type_filling'] = mt5.ORDER_FILLING_IOC
     else:
         print("Choose a valid order type from SELL_STOP, BUY_STOP, SELL, BUY")
         raise SyntaxError
 
     if direct is True:
         # Send the order to MT5
-        order_result = MetaTrader5.order_send(request)
+        order_result = mt5.order_send(request)
         # Notify based on return outcomes
         if order_result[0] == 10009:
             # Print result
@@ -66,15 +67,13 @@ def place_order(order_type, symbol, volume, stop_loss, take_profit, comment, dir
         elif order_result[0] == 10027:
             # Turn off autotrading
             print(f"Turn off Algo Trading on MT5 Terminal")
-            raise exceptions.MetaTraderAlgoTradingNotDisabledError
         else:
             # Print result
             print(f"Error placing order. ErrorCode {order_result[0]}, Error Details: {order_result}")
-            raise exceptions.MetaTraderOrderPlacingError
 
     else:
         # Check the order
-        result = MetaTrader5.order_check(request)
+        result = mt5.order_check(request)
         if result[0] == 0:
             # print("Balance Check Successful") # Enable to error check Balance Check
             # If order check is successful, place the order. Little bit of recursion for fun.
@@ -90,7 +89,7 @@ def place_order(order_type, symbol, volume, stop_loss, take_profit, comment, dir
             )
         else:
             print(f"Order unsucessful. Details: {result}")
-            raise exceptions.MetaTraderOrderCheckError
+            raise mt5.MetaTraderOrderCheckError
 
 # Function to cancel an order
 def cancel_order(order_number):
@@ -101,18 +100,16 @@ def cancel_order(order_number):
     """
     # Create the request
     request = {
-        "action": MetaTrader5.TRADE_ACTION_REMOVE,
+        "action": mt5.TRADE_ACTION_REMOVE,
         "order": order_number,
         "comment": "Order Removed"
     }
     # Send order to MT5
-    order_result = MetaTrader5.order_send(request)
+    order_result = mt5.order_send(request)
     if order_result[0] == 10009:
         return True
     else:
         print(f"Error cancelling order. Details: {order_result}")
-        raise exceptions.MetaTraderCancelOrderError
-
 
 # Function to modify an open position
 def modify_position(order_number, symbol, new_stop_loss, new_take_profit):
@@ -126,19 +123,19 @@ def modify_position(order_number, symbol, new_stop_loss, new_take_profit):
     """
     # Create the request
     request = {
-        "action": MetaTrader5.TRADE_ACTION_SLTP,
+        "action": mt5.TRADE_ACTION_SLTP,
         "symbol": symbol,
         "sl": new_stop_loss,
         "tp": new_take_profit,
         "position": order_number
     }
     # Send order to MT5
-    order_result = MetaTrader5.order_send(request)
+    order_result = mt5.order_send(request)
     if order_result[0] == 10009:
         return True
     else:
         print(f"Error modifying position. Details: {order_result}")
-        raise exceptions.MetaTraderModifyPositionError
+        raise mt5.MetaTraderModifyPositionError
 
 
 # Function to retrieve all open orders from MT5
@@ -147,7 +144,7 @@ def get_open_orders():
     Function to retrieve a list of open orders from MetaTrader 5
     :return: List of open orders
     """
-    orders = MetaTrader5.orders_get()
+    orders = mt5.orders_get()
     order_array = []
     for order in orders:
         order_array.append(order[0])
@@ -161,7 +158,7 @@ def get_open_positions():
     :return: list of positions
     """
     # Get position objects
-    positions = MetaTrader5.positions_get()
+    positions = mt5.positions_get()
     # Return position objects
     return positions
 
@@ -175,91 +172,30 @@ def close_position(order_number, symbol, volume, order_type, price, comment):
     """
     # Create the request
     request = {
-        'action': MetaTrader5.TRADE_ACTION_DEAL,
+        'action': mt5.TRADE_ACTION_DEAL,
         'symbol': symbol,
         'volume': volume,
         'position': order_number,
         'price': price,
-        'type_time': MetaTrader5.ORDER_TIME_GTC,
-        'type_filling': MetaTrader5.ORDER_FILLING_IOC,
+        'type_time': mt5.ORDER_TIME_GTC,
+        'type_filling': mt5.ORDER_FILLING_IOC,
         'comment': comment
     }
 
     if order_type == "SELL":
-        request['type'] = MetaTrader5.ORDER_TYPE_SELL
+        request['type'] = mt5.ORDER_TYPE_SELL
     elif order_type == "BUY":
-        request['type'] = MetaTrader5.ORDER_TYPE_BUY
+        request['type'] = mt5.ORDER_TYPE_BUY
     else:
         print(f"Incorrect syntax for position close {order_type}")
         raise SyntaxError
 
     # Place the order
-    result = MetaTrader5.order_send(request)
+    result = mt5.order_send(request)
     if result[0] == 10009:
         return True
     else:
         print(f"Error closing position. Details: {result}")
-        raise exceptions.MetaTraderClosePositionError
-
-
-# Function to convert a timeframe string in MetaTrader 5 friendly format
-def set_query_timeframe(timeframe):
-    # Implement a Pseudo Switch statement. Note that Python 3.10 implements match / case but have kept it this way for
-    # backwards integration
-    if timeframe == "M1":
-        return MetaTrader5.TIMEFRAME_M1
-    elif timeframe == "M2":
-        return MetaTrader5.TIMEFRAME_M2
-    elif timeframe == "M3":
-        return MetaTrader5.TIMEFRAME_M3
-    elif timeframe == "M4":
-        return MetaTrader5.TIMEFRAME_M4
-    elif timeframe == "M5":
-        return MetaTrader5.TIMEFRAME_M5
-    elif timeframe == "M6":
-        return MetaTrader5.TIMEFRAME_M6
-    elif timeframe == "M10":
-        return MetaTrader5.TIMEFRAME_M10
-    elif timeframe == "M12":
-        return MetaTrader5.TIMEFRAME_M12
-    elif timeframe == "M15":
-        return MetaTrader5.TIMEFRAME_M15
-    elif timeframe == "M20":
-        return MetaTrader5.TIMEFRAME_M20
-    elif timeframe == "M30":
-        return MetaTrader5.TIMEFRAME_M30
-    elif timeframe == "H1":
-        return MetaTrader5.TIMEFRAME_H1
-    elif timeframe == "H2":
-        return MetaTrader5.TIMEFRAME_H2
-    elif timeframe == "H3":
-        return MetaTrader5.TIMEFRAME_H3
-    elif timeframe == "H4":
-        return MetaTrader5.TIMEFRAME_H4
-    elif timeframe == "H6":
-        return MetaTrader5.TIMEFRAME_H6
-    elif timeframe == "H8":
-        return MetaTrader5.TIMEFRAME_H8
-    elif timeframe == "H12":
-        return MetaTrader5.TIMEFRAME_H12
-    elif timeframe == "D1":
-        return MetaTrader5.TIMEFRAME_D1
-    elif timeframe == "W1":
-        return MetaTrader5.TIMEFRAME_W1
-    elif timeframe == "MN1":
-        return MetaTrader5.TIMEFRAME_MN1
-    else:
-        print(f"Incorrect timeframe provided. {timeframe}")
-        raise ValueError
-
-
-# Function to query previous candlestick data from MT5
-def query_historic_data(symbol, timeframe, number_of_candles):
-    # Convert the timeframe into an MT5 friendly format
-    mt5_timeframe = set_query_timeframe(timeframe)
-    # Retrieve data from MT5
-    rates = MetaTrader5.copy_rates_from_pos(symbol, mt5_timeframe, 1, number_of_candles)
-    return rates
 
 
 # Function to retrieve latest tick for a symbol
@@ -270,68 +206,7 @@ def retrieve_latest_tick(symbol):
     :return: Dictionary object
     """
     # Retrieve the tick information
-    tick = MetaTrader5.symbol_info_tick(symbol)._asdict()
+    tick = mt5.symbol_info_tick(symbol)._asdict()
     spread = tick['ask'] - tick['bid']
     tick['spread'] = spread
     return tick
-
-
-# Function to retrieve ticks from a time range
-def retrieve_tick_time_range(start_time_utc, finish_time_utc, symbol, dataframe=False):
-    # Set option in MT5 terminal for Unlimited bars
-    # Check time format of start time
-    if type(start_time_utc) != datetime.datetime:
-        print(f"Time range tick start time is in incorrect format")
-        raise ValueError
-    # Check time format of finish time
-    if type(finish_time_utc) != datetime.datetime:
-        print(f"Time range tick finish time is in incorrect format")
-        raise ValueError
-    # Retrieve ticks
-    ticks = MetaTrader5.copy_ticks_range(symbol, start_time_utc, finish_time_utc, MetaTrader5.COPY_TICKS_INFO)
-    # Convert into dataframe only if Dataframe set to True
-    if dataframe:
-        # Convert into a dataframe
-        ticks_data_frame = pandas.DataFrame(ticks)
-        # Add spread
-        ticks_data_frame['spread'] = ticks_data_frame['ask'] - ticks_data_frame['bid']
-        # Add symbol
-        ticks_data_frame['symbol'] = symbol
-        # Format integers into signed integers (postgres doesn't support unsigned int)
-        ticks_data_frame['time'] = ticks_data_frame['time'].astype('int64')
-        ticks_data_frame['volume'] = ticks_data_frame['volume'].astype('int64')
-        ticks_data_frame['time_msc'] = ticks_data_frame['time_msc'].astype('int64')
-        return ticks_data_frame
-    return ticks
-
-
-# Function to retrieve candlestick data for a specified time range
-def retrieve_candlestick_data_range(start_time_utc, finish_time_utc, symbol, timeframe, dataframe=False):
-    # Set option in MT5 terminal for Unlimited bars
-    # Check time format of start time
-    if type(start_time_utc) != datetime.datetime:
-        print(f"Time range tick start time is in incorrect format")
-        raise ValueError
-    # Check time format of finish time
-    if type(finish_time_utc) != datetime.datetime:
-        print(f"Time range tick finish time is in incorrect format")
-        raise ValueError
-    # Convert the timeframe into MT5 compatible format
-    timeframe_value = set_query_timeframe(timeframe)
-    # Retrieve the data
-    candlestick_data = MetaTrader5.copy_rates_range(symbol, timeframe_value, start_time_utc, finish_time_utc)
-    if dataframe:
-        # Convert to a dataframe
-        candlestick_dataframe = pandas.DataFrame(candlestick_data)
-        # Add in symbol and timeframe columns
-        candlestick_dataframe['symbol'] = symbol
-        candlestick_dataframe['timeframe'] = timeframe
-        # Convert integers into signed integers (postgres doesn't support unsigned int)
-        candlestick_dataframe['time'] = candlestick_dataframe['time'].astype('int64')
-        candlestick_dataframe['tick_volume'] = candlestick_dataframe['tick_volume'].astype('float64')
-        candlestick_dataframe['spread'] = candlestick_dataframe['spread'].astype('int64')
-        candlestick_dataframe['real_volume'] = candlestick_dataframe['real_volume'].astype('int64')
-        # Return completed dataframe
-        return candlestick_dataframe
-    else:
-        return candlestick_data
