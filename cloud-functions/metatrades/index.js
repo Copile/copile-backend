63605843acf60c8a34097231783421f3153b71d0
Vantria <s1spam118@gmail.com>
2024-03-03 22:21:47 +0000

    revert to old metatrades cf

--- cloud-functions/metatrades/index.js
+++ cloud-functions/metatrades/index.js
@@ -23,11 +23,13 @@ app.get("/trades", async (req, res) => {
     // Wait for all queries to complete
     const results = await Promise.all(tradeQueries);
 
-    const tradePromises = results.flatMap((tradeQuery, index) =>
-      tradeQuery.docs.map(async (doc) => {
-        const tradeId = tradeIds[index];
-        console.log(`Found ${tradeQuery.docs.length} trades with tradeID: ${tradeId}`);
+    // Process each query result
+    const trades = [];
+    results.forEach((tradeQuery, index) => {
+      const tradeId = tradeIds[index];
+      console.log(`Found ${tradeQuery.docs.length} trades with tradeID: ${tradeId}`);
 
+      tradeQuery.docs.forEach((doc) => {
         const parentId = doc.ref.parent.parent.id;
         console.log(`Processing trade with parentId: ${parentId} and traderId: ${traderId}`);
 
@@ -36,34 +38,12 @@ app.get("/trades", async (req, res) => {
             `Trade with parentId: ${parentId} is NOT equal to traderId: ${traderId}, adding to response`
           );
 
-          // Utilize promises to fetch trader document, stop-losses, and take-profits in parallel
-          const [traderDocSnapshot, stopLossesSnapshot, takeProfitsSnapshot] = await Promise.all([
-            doc.ref.parent.parent.get(),
-            doc.ref.collection("stop-losses").get(),
-            doc.ref.collection("take-profits").get(),
-          ]);
-
-          const accountNickname = traderDocSnapshot.data().nickname;
-
-          // Map documents in each collection to their data
-          const stopLosses = stopLossesSnapshot.docs.map((doc) => doc.data());
-          const takeProfits = takeProfitsSnapshot.docs.map((doc) => doc.data());
-
-          const tradeData = {
-            ...doc.data(),
-            account_nickname: accountNickname,
-            stop_losses: stopLosses,
-            take_profits: takeProfits,
-          };
-          return tradeData;
+          trades.push(doc.data());
         } else {
           console.log(`Trade with parentId: ${parentId} is equal to traderId: ${traderId}, skipping`);
-          return null; // Return null for trades that should be skipped
         }
-      })
-    );
-    // Wait for all the trade data promises to resolve and filter out nulls
-    const trades = (await Promise.all(tradePromises)).filter((trade) => trade !== null);
+      });
+    });
 
     console.log(`Sending response with meta_trades:`, trades);
     res.status(200).json(trades);
@@ -72,53 +52,6 @@ app.get("/trades", async (req, res) => {
     res.status(500).send("Error fetching meta trades");
   }
 });
-
-// app.get("/trades", async (req, res) => {
-//   console.log("Received request for /trades with body:", req.body);
-//   const tradeIds = req.query.ids.split(",");
-//   const traderId = req.get("traderId");
-
-//   try {
-//     const tradesRef = db.collectionGroup("trades");
-
-//     // Map each tradeId to a promise that queries for that tradeId
-//     const tradeQueries = tradeIds.map((tradeId) => {
-//       console.log(`Looking for trades with tradeID: ${tradeId}`);
-//       return tradesRef.where("tradeID", "==", tradeId).get();
-//     });
-
-//     // Wait for all queries to complete
-//     const results = await Promise.all(tradeQueries);
-
-//     // Process each query result
-//     const trades = [];
-//     results.forEach((tradeQuery, index) => {
-//       const tradeId = tradeIds[index];
-//       console.log(`Found ${tradeQuery.docs.length} trades with tradeID: ${tradeId}`);
-
-//       tradeQuery.docs.forEach((doc) => {
-//         const parentId = doc.ref.parent.parent.id;
-//         console.log(`Processing trade with parentId: ${parentId} and traderId: ${traderId}`);
-
-//         if (parentId !== traderId) {
-//           console.log(
-//             `Trade with parentId: ${parentId} is NOT equal to traderId: ${traderId}, adding to response`
-//           );
-
-//           trades.push(doc.data());
-//         } else {
-//           console.log(`Trade with parentId: ${parentId} is equal to traderId: ${traderId}, skipping`);
-//         }
-//       });
-//     });
-
-//     console.log(`Sending response with meta_trades:`, trades);
-//     res.status(200).json(trades);
-//   } catch (error) {
-//     console.error("Error fetching meta trades:", error);
-//     res.status(500).send("Error fetching meta trades");
-//   }
-// });
 
 app.get("/", (req, res) => {
   res.send("Copile Trader API");
