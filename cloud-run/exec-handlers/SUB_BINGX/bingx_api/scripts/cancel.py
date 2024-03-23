56ab298f8c2919ab37c8a3c5492bfe4d0dc8e472
Aldin Malic <aldin.malic@tripmakery.com>
2024-03-23 17:51:34 +0100

    variable issue

--- cloud-run/exec-handlers/SUB_BINGX/bingx_api/scripts/cancel.py
+++ cloud-run/exec-handlers/SUB_BINGX/bingx_api/scripts/cancel.py
@@ -4,6 +4,6 @@ async def send_cancel(session, symbol, user_id, trader_id, trade_id, document_id
     order = await get_specific_order(trader_id, user_id, trade_id, document_id, trade_type)
 
     await session.cancel_order(symbol, order['orderID'], None)
-    await delete_tp_sl_order(user_id, trade_id, document_id, trade_type)
+    await delete_tp_sl_order(trader_id, user_id, trade_id, document_id, trade_type)
 
     return
